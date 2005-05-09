c0415152c4cd3b8d90514305cad704f22273813b
etremblay <etremblay@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-05-09 22:55:52 +0000

    Added the class to implement the perspective with the icon for it.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@118 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ src/seg/jUCMNav/views/UCMPerspectiveFactory.java
@@ -0,0 +1,21 @@
+package seg.jUCMNav.views;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * Created 2005-05-09
+ * 
+ * @author Etienne Tremblay
+ */
+public class UCMPerspectiveFactory implements IPerspectiveFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
