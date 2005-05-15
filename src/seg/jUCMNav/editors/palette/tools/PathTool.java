91baff289c23ef0faa9a60c8d656247f60c60675
etremblay <etremblay@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-05-15 04:52:25 +0000

    PathTool seems to be working flawlessly for creating new path, then extending it...  Click on any connection and an EmptyPoint will get created on the path.  If you click on a connection of an other path, the EndPoint of this path will get selected so that you can continue to extend this path easily.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@148 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/editors/palette/tools/PathTool.java
+++ src/seg/jUCMNav/editors/palette/tools/PathTool.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartViewer;
+import org.eclipse.gef.Request;
 import org.eclipse.gef.requests.CreationFactory;
 import org.eclipse.gef.tools.CreationTool;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -13,9 +14,7 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 import seg.jUCMNav.model.ModelCreationFactory;
 import ucm.map.EmptyPoint;
 import ucm.map.EndPoint;
-import ucm.map.Map;
 import ucm.map.NodeConnection;
-import ucm.map.PathGraph;
 import ucm.map.PathNode;
 import ucm.map.StartPoint;
 import urn.URNspec;
@@ -31,12 +30,17 @@ public class PathTool extends CreationTool implements ISelectionChangedListener 
 	private final int STARTPOINT = 1;
 	private final int NOSELECT = 2;
 	private final int EMPTYPOINT = 3;
+	private final int CONNECTION = 4;
 
 	private int state = NOSELECT;
 
 	private URNspec urn;
 
 	private EditPart target;
+	
+	private Request targetRequest;
+	
+	private EditPart selected;
 
 	/**
 	 *  
@@ -52,10 +56,33 @@ public class PathTool extends CreationTool implements ISelectionChangedListener 
 		super();
 		this.urn = urn;
 	}
+	
+	protected Request createTargetRequest() {
+		return super.createTargetRequest();
+	}
+	
+	protected void setTargetRequest(Request req) {
+		targetRequest = req;
+		super.setTargetRequest(req);
+	}
 
 	protected void setTargetEditPart(EditPart editpart) {
-		target = editpart;
+		if(target != editpart){
+			target = editpart;
+			setState();
+			if(target != null)
+				setTargetRequest(createTargetRequest());
+		}
 		super.setTargetEditPart(editpart);
+	}
+	
+	protected boolean handleMove() {
+		// Change the target under the mouse before updating the target request...
+		updateTargetUnderMouse();
+		updateTargetRequest();
+		setCurrentCommand(getCommand());
+		showTargetFeedback();
+		return true;
 	}
 
 	public void setViewer(EditPartViewer viewer) {
@@ -71,16 +98,25 @@ public class PathTool extends CreationTool implements ISelectionChangedListener 
 	}
 
 	protected CreationFactory getFactory() {
+		ModelCreationFactory factory;
 		switch (state) {
 		case ENDPOINT:
-			return new ModelCreationFactory(getURNspec(), EndPoint.class);
+			factory = new ModelCreationFactory(getURNspec(), EndPoint.class);
+			break;
 		case STARTPOINT:
-			return new ModelCreationFactory(getURNspec(), StartPoint.class);
+			factory = new ModelCreationFactory(getURNspec(), StartPoint.class);
+			break;
 		case NOSELECT:
-			return new ModelCreationFactory(getURNspec(), StartPoint.class);
+			factory = new ModelCreationFactory(getURNspec(), StartPoint.class);
+			break;
+		case CONNECTION:
+			factory = new ModelCreationFactory(getURNspec(), EmptyPoint.class);
+			break;
 		default:
-			return new ModelCreationFactory(getURNspec(), EmptyPoint.class);
+			factory = new ModelCreationFactory(getURNspec(), StartPoint.class);
 		}
+		
+		return factory;
 	}
 
 	protected URNspec getURNspec() {
@@ -104,15 +140,29 @@ public class PathTool extends CreationTool implements ISelectionChangedListener 
 		List list = selecteds.toList();
 
 		if (list.size() == 1) {
-			EditPart selected = (EditPart) (list.get(0));
-
-			if (selected.getModel() instanceof EndPoint) {
-				state = ENDPOINT;
-			} else if (selected.getModel() instanceof StartPoint) {
-				state = STARTPOINT;
-			} else if (selected.getModel() instanceof PathGraph || selected.getModel() instanceof Map) {
-				state = NOSELECT;
+			selected = (EditPart) (list.get(0));
+			
+			setState();
+			
+			// Regenerate the request depending on the state
+			createTargetRequest();
+		}
+	}
+	
+	protected void setState(){
+		if(target != null) {
+			if(target.getModel() instanceof NodeConnection){
+				state = CONNECTION;
+				return;
 			}
+		}
+
+		if (selected.getModel() instanceof EndPoint) {
+			state = ENDPOINT;
+		} else if (selected.getModel() instanceof StartPoint) {
+			state = STARTPOINT;
+		} else {
+			state = NOSELECT;
 		}
 	}
 
@@ -137,8 +187,24 @@ public class PathTool extends CreationTool implements ISelectionChangedListener 
 					selectModelElement(end);
 			} else
 				selectModelElement(start);
-		} else
+		} else if(model instanceof EmptyPoint){
+			selectModelElement(findEndPoint((PathNode)model));
+		}else
 			selectModelElement(model);
+	}
+	
+	private PathNode findEndPoint(PathNode start){
+		PathNode node = null;
+		PathNode end = null;
+		end = start;
+		if(end instanceof EndPoint)
+			return end;
+		
+		while(!(end instanceof EndPoint)){
+			end = (PathNode) ((NodeConnection) end.getSucc().get(0)).getTarget();
+		}
+		
+		return end;
 	}
 
 	protected void selectModelElement(Object model) {
