c5de9377a136eec425371d73d1f7474e397557b1
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-05-24 22:01:24 +0000

    bug 249 - hover feedback on pathnodes. is it okay?
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@211 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/editpolicies/feedback/PathNodeNonResizableEditPolicy.java
+++ src/seg/jUCMNav/editpolicies/feedback/PathNodeNonResizableEditPolicy.java
@@ -4,49 +4,70 @@
  */
 package seg.jUCMNav.editpolicies.feedback;
 
+import org.eclipse.gef.Request;
 import org.eclipse.gef.editpolicies.SelectionEditPolicy;
 import org.eclipse.swt.graphics.Color;
 
 import seg.jUCMNav.editparts.PathNodeEditPart;
 import seg.jUCMNav.figures.PathNodeFigure;
+import seg.jUCMNav.figures.Polygon;
 
 /**
  * Created 2005-03-04
  * 
  * @author Etienne Tremblay
  */
-public class PathNodeNonResizableEditPolicy extends SelectionEditPolicy  {
+public class PathNodeNonResizableEditPolicy extends SelectionEditPolicy {
 
-	/**
-	 * 
-	 */
-	public PathNodeNonResizableEditPolicy() {
-		super();
-	}
-	
+    /**
+     *  
+     */
+    public PathNodeNonResizableEditPolicy() {
+        super();
+    }
 
-	private PathNodeFigure getFigure(){
-		return (PathNodeFigure)((PathNodeEditPart)this.getHost()).getFigure();
-	}
+    private PathNodeFigure getFigure() {
+        return (PathNodeFigure) ((PathNodeEditPart) this.getHost()).getFigure();
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.gef.editpolicies.SelectionEditPolicy#hideSelection()
-	 */
-	protected void hideSelection() {
-	  
-		getFigure().setBackgroundColor(new Color(null, 255, 255, 255));
-		getFigure().setSelected(false);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.editpolicies.SelectionEditPolicy#hideSelection()
+     */
+    protected void hideSelection() {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.gef.editpolicies.SelectionEditPolicy#showSelection()
-	 */
-	protected void showSelection() {
-	  
-		getFigure().setBackgroundColor(new Color(null, 0, 102, 204));
-		getFigure().setSelected(true);
-	}
+        getFigure().setBackgroundColor(new Color(null, 255, 255, 255));
+        getFigure().setSelected(false);
+    }
 
-	
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.editpolicies.SelectionEditPolicy#showSelection()
+     */
+    protected void showSelection() {
+
+        getFigure().setBackgroundColor(new Color(null, 0, 102, 204));
+        getFigure().setSelected(true);
+    }
+
+    public void eraseTargetFeedback(Request request) {
+        getFeedbackLayer().getChildren().clear();
+        getFeedbackLayer().erase();
+    }
+
+    public void showTargetFeedback(Request request) {
+        Polygon p = new Polygon();
+        p.addPoint(getFigure().getBounds().getTopLeft());
+        p.addPoint(getFigure().getBounds().getTopRight());
+        p.addPoint(getFigure().getBounds().getBottomRight());
+        p.addPoint(getFigure().getBounds().getBottomLeft());
+        p.setFillXOR(true);
+        
+        if (getFeedbackLayer().getChildren().size()==0)
+        getFeedbackLayer().add(p);
+        getFeedbackLayer().invalidate();
+    }
 
 }
