b66bb08be00cb01eb0f349089c8edb24faf1d18a
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-05-27 12:43:38 +0000

    improved feedback on labels.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@245 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/editpolicies/feedback/LabelFeedbackEditPolicy.java
+++ src/seg/jUCMNav/editpolicies/feedback/LabelFeedbackEditPolicy.java
@@ -6,11 +6,16 @@ package seg.jUCMNav.editpolicies.feedback;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Polyline;
+import org.eclipse.draw2d.RoundedRectangle;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.Request;
+import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.gef.editpolicies.GraphicalEditPolicy;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 
 import seg.jUCMNav.editparts.LabelEditPart;
 import seg.jUCMNav.editparts.PathNodeEditPart;
@@ -32,6 +37,7 @@ import urncore.UCMmodelElement;
 public class LabelFeedbackEditPolicy extends GraphicalEditPolicy {
 
     Polyline line;
+    RoundedRectangle roundrect;
 
     /**
      *  
@@ -59,20 +65,27 @@ public class LabelFeedbackEditPolicy extends GraphicalEditPolicy {
             getFeedbackLayer().remove(line);
             line = null;
         }
+        if (roundrect != null) {
+            getFeedbackLayer().remove(roundrect);
+            roundrect = null;
+        }
     }
 
     public void showTargetFeedback(Request request) {
-        if (line == null) {
+        if (line == null && roundrect == null) {
+            // we need to scale our feedback.
+            double zoomLevel = ((ZoomManager) ((ScrollingGraphicalViewer) getHost().getViewer()).getProperty(ZoomManager.class.toString())).getZoom();
 
             line = new Polyline();
             PointList pl = new PointList();
+
+            // get the center of the label.
             Point pt = getFigure().getLocation();
             pt.x += getFigure().getBounds().width / 2;
             pt.y += getFigure().getBounds().height / 2;
-            Point pt2 = null;
-            //pl.addPoint(getFigure().getLocation());
-            //pl.addPoint(pt);
 
+            // get the pathnode/component ref's figure
+            Point pt2 = null;
             Rectangle rect = null;
             if (getReference() instanceof ComponentRef) {
                 ComponentRef cr = (ComponentRef) getReference();
@@ -80,6 +93,8 @@ public class LabelFeedbackEditPolicy extends GraphicalEditPolicy {
             } else if (getReference() instanceof PathNode) {
                 PathNode pn = (PathNode) getReference();
                 try {
+                    // we're looking for the first child so that we don't point to the external bounding box; we want the real figure.
+                    // TODO: adjust when there are no children.
                     rect = ((IFigure) ((PathNodeEditPart) getHost().getViewer().getEditPartRegistry().get(pn)).getNodeFigure().getChildren().get(0))
                             .getBounds();
                 } catch (Exception ex) {
@@ -88,9 +103,12 @@ public class LabelFeedbackEditPolicy extends GraphicalEditPolicy {
                 pt2 = new Point(pn.getX(), pn.getY());
             }
 
+            // calculate the differences.
             int diffx = pt.x - pt2.x;
             int diffy = pt.y - pt2.y;
 
+            // put the lines on the bounds.
+            // TODO: improve positioning on center. must never be exact center but should always have either x or y positioned at center.
             if (diffx != 0 && Math.abs(diffx) > getFigure().getBounds().width) {
                 pt.x -= (Math.abs(diffx) / diffx) * getFigure().getBounds().width / 2;
                 if (rect != null)
@@ -103,10 +121,26 @@ public class LabelFeedbackEditPolicy extends GraphicalEditPolicy {
                     pt2.y += (Math.abs(diffy) / diffy) * rect.height / 2;
             }
 
+            // scale
+            pt.scale(zoomLevel);
+            pt2.scale(zoomLevel);
             pl.addPoint(pt);
             pl.addPoint(pt2);
 
+            // style
             line.setPoints(pl);
+            line.setForegroundColor(new Color(null, 180, 180, 180));
+            line.setLineStyle(SWT.LINE_DOT);
+
+            // rectangle over label.
+            roundrect = new RoundedRectangle();
+            roundrect.setFill(false);
+            roundrect.setBounds(getFigure().getBounds().getCopy().scale(zoomLevel));
+            roundrect.setForegroundColor(line.getForegroundColor());
+            roundrect.setLineStyle(SWT.LINE_DOT);
+
+            // add feedback
+            getFeedbackLayer().add(roundrect);
             getFeedbackLayer().add(line);
         }
     }
