a06f4c7b99a26ab1be2d25691afbdd06369d67b3
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-05-31 14:07:05 +0000

    bug 260 - labels now display associated element's properties.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@267 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/editparts/LabelEditPart.java
+++ src/seg/jUCMNav/editparts/LabelEditPart.java
@@ -8,6 +8,7 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.Request;
@@ -16,6 +17,7 @@ import org.eclipse.gef.requests.DirectEditRequest;
 import org.eclipse.gef.tools.DirectEditManager;
 import org.eclipse.jface.viewers.ICellEditorValidator;
 import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.ui.views.properties.IPropertySource;
 
 import seg.jUCMNav.editpolicies.directEditPolicy.ExtendedDirectEditManager;
 import seg.jUCMNav.editpolicies.directEditPolicy.LabelCellEditorLocator;
@@ -25,6 +27,7 @@ import seg.jUCMNav.editpolicies.feedback.LabelFeedbackEditPolicy;
 import seg.jUCMNav.figures.EditableLabel;
 import seg.jUCMNav.figures.LabelFigure;
 import seg.jUCMNav.figures.util.JUCMNavFigure;
+import seg.jUCMNav.views.property.LabelPropertySource;
 import ucm.map.ComponentRef;
 import ucm.map.MapPackage;
 import ucm.map.PathNode;
@@ -42,20 +45,14 @@ import urncore.UCMmodelElement;
  * @author Jordan McManus
  */
 public class LabelEditPart extends ModelElementEditPart {
-    private UCMmodelElement modelElement;
-    private ComponentElement comp = null;
-    private Responsibility resp = null;
 
     private static final int LABEL_PADDING_X = 6;
     private static final int LABEL_PADDING_Y = 4;
+    private ComponentElement comp = null;
 
     protected DirectEditManager manager;
-
-    public LabelEditPart(Label model, UCMmodelElement modelElement) {
-        super();
-        setModel(model);
-        this.modelElement = modelElement;
-    }
+    private UCMmodelElement modelElement;
+    private Responsibility resp = null;
 
     public LabelEditPart(Label model) {
         super();
@@ -65,6 +62,12 @@ public class LabelEditPart extends ModelElementEditPart {
         else if (model instanceof ComponentLabel)
             modelElement = ((ComponentLabel) model).getCompRef();
 
+    }
+
+    public LabelEditPart(Label model, UCMmodelElement modelElement) {
+        super();
+        setModel(model);
+        this.modelElement = modelElement;
     }
 
     /*
@@ -88,6 +91,44 @@ public class LabelEditPart extends ModelElementEditPart {
         super.activate();
     }
 
+    private Point calculateModelElementPosition(Label label, Dimension labelDimension) {
+        Point location;
+
+        if (modelElement instanceof PathNode) {
+            PathNode node = (PathNode) modelElement;
+            location = new Point(node.getX() - labelDimension.width / 2 - label.getDeltaX(), node.getY()
+                    - (labelDimension.height + JUCMNavFigure.getDimension(modelElement).height / 2) - label.getDeltaY());
+        } else if (modelElement instanceof ComponentRef) {
+            ComponentRef component = (ComponentRef) modelElement;
+            location = new Point(component.getX() - label.getDeltaX(), component.getY() - label.getDeltaY() - labelDimension.height);
+        } else {
+            location = new Point(0, 0);
+        }
+
+        return location;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
+     */
+    protected void createEditPolicies() {
+        installEditPolicy(EditPolicy.COMPONENT_ROLE, new LabelComponentEditPolicy());
+        installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE, new LabelDirectEditPolicy());
+        installEditPolicy(EditPolicy.SELECTION_FEEDBACK_ROLE, new LabelFeedbackEditPolicy());
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.editparts.AbstractGraphicalEditPart#createFigure()
+     */
+    protected IFigure createFigure() {
+        EditableLabel label = new EditableLabel("");
+        return new LabelFigure(label);
+    }
+
     /*
      * (non-Javadoc)
      * 
@@ -109,6 +150,25 @@ public class LabelEditPart extends ModelElementEditPart {
         super.deactivate();
     }
 
+    /**
+     * For direct edit, verify location.
+     * 
+     * @param requestLoc
+     * @return
+     */
+    private boolean directEditHitTest(Point requestLoc) {
+        LabelFigure figure = (LabelFigure) getFigure();
+        EditableLabel nameLabel = figure.getLabel();
+        nameLabel.translateToRelative(requestLoc);
+        if (nameLabel.containsPoint(requestLoc))
+            return true;
+        return false;
+    }
+
+    public LabelFigure getLabelFigure() {
+        return (LabelFigure) getFigure();
+    }
+
     /*
      * (non-Javadoc)
      * 
@@ -116,6 +176,13 @@ public class LabelEditPart extends ModelElementEditPart {
      */
     public Label getModelObj() {
         return (Label) getModel();
+    }
+
+    protected IPropertySource getPropertySource() {
+        if (propertySource == null) {
+            propertySource = new LabelPropertySource((EObject) getModel());
+        }
+        return propertySource;
     }
 
     /*
@@ -127,29 +194,80 @@ public class LabelEditPart extends ModelElementEditPart {
         return modelElement;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.gef.editparts.AbstractGraphicalEditPart#createFigure()
+    /**
+     * @param handles
+     *            the name change during an edit
      */
-    protected IFigure createFigure() {
-        EditableLabel label = new EditableLabel("");
-        return new LabelFigure(label);
+    public void handleNameChange(String value) {
+        LabelFigure tableFigure = (LabelFigure) getFigure();
+        EditableLabel label = tableFigure.getLabel();
+        label.setVisible(false);
+        refreshVisuals();
     }
 
     /*
      * (non-Javadoc)
      * 
-     * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
+     * @see org.eclipse.emf.common.notify.Adapter#notifyChanged(org.eclipse.emf.common.notify.Notification)
      */
-    protected void createEditPolicies() {
-        installEditPolicy(EditPolicy.COMPONENT_ROLE, new LabelComponentEditPolicy());
-        installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE, new LabelDirectEditPolicy());
-        installEditPolicy(EditPolicy.SELECTION_FEEDBACK_ROLE, new LabelFeedbackEditPolicy());
+    public void notifyChanged(Notification notification) {
+        int type = notification.getEventType();
+        int featureId = notification.getFeatureID(MapPackage.class);
+
+        if (type == Notification.SET) {
+            if (featureId == MapPackage.COMPONENT_REF__COMP_DEF) {
+                if (modelElement instanceof ComponentRef) {
+                    if (comp != null)
+                        comp.eAdapters().remove(this);
+                    comp = ((ComponentRef) modelElement).getCompDef();
+                    if (comp != null)
+                        comp.eAdapters().add(this);
+                }
+            } else if (featureId == MapPackage.RESP_REF__RESP_DEF)
+                if (modelElement instanceof RespRef) {
+                    if (resp != null)
+                        resp.eAdapters().remove(this);
+                    resp = ((RespRef) modelElement).getRespDef();
+                    if (resp != null)
+                        resp.eAdapters().add(this);
+                }
+        }
+
+        if (getParent() != null) {
+            ((MapAndPathGraphEditPart) getParent()).notifyChanged(notification);
+            refreshVisuals();
+        }
     }
 
-    public LabelFigure getLabelFigure() {
-        return (LabelFigure) getFigure();
+    /**
+     * Opens the direct edit manager.
+     *  
+     */
+    protected void performDirectEdit() {
+        if (manager == null) {
+            LabelFigure figure = (LabelFigure) getFigure();
+            EditableLabel nameLabel = figure.getLabel();
+
+            ICellEditorValidator validator = new ICellEditorValidator() {
+                public String isValid(Object value) {
+                    return "";
+                }
+            };
+
+            manager = new ExtendedDirectEditManager(this, TextCellEditor.class, new LabelCellEditorLocator(nameLabel), nameLabel, validator);
+        }
+        manager.show();
+    }
+
+    /**
+     * Show direct edit on label on double click, f2 or delay.
+     */
+    public void performRequest(Request request) {
+        if (request.getType() == RequestConstants.REQ_DIRECT_EDIT || request.getType() == RequestConstants.REQ_OPEN) {
+            if (request instanceof DirectEditRequest && !directEditHitTest(((DirectEditRequest) request).getLocation().getCopy()))
+                return;
+            performDirectEdit();
+        }
     }
 
     /*
@@ -193,114 +311,6 @@ public class LabelEditPart extends ModelElementEditPart {
             }
 
         }
-    }
-
-    private Point calculateModelElementPosition(Label label, Dimension labelDimension) {
-        Point location;
-
-        if (modelElement instanceof PathNode) {
-            PathNode node = (PathNode) modelElement;
-            location = new Point(node.getX() - labelDimension.width / 2 - label.getDeltaX(), node.getY()
-                    - (labelDimension.height + JUCMNavFigure.getDimension(modelElement).height / 2) - label.getDeltaY());
-        } else if (modelElement instanceof ComponentRef) {
-            ComponentRef component = (ComponentRef) modelElement;
-            location = new Point(component.getX() - label.getDeltaX(), component.getY() - label.getDeltaY() - labelDimension.height);
-        } else {
-            location = new Point(0, 0);
-        }
-
-        return location;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.emf.common.notify.Adapter#notifyChanged(org.eclipse.emf.common.notify.Notification)
-     */
-    public void notifyChanged(Notification notification) {
-        int type = notification.getEventType();
-        int featureId = notification.getFeatureID(MapPackage.class);
-
-        if (type == Notification.SET) {
-            if (featureId == MapPackage.COMPONENT_REF__COMP_DEF) {
-                if (modelElement instanceof ComponentRef) {
-                    if (comp != null)
-                        comp.eAdapters().remove(this);
-                    comp = ((ComponentRef) modelElement).getCompDef();
-                    if (comp != null)
-                        comp.eAdapters().add(this);
-                }
-            } else if (featureId == MapPackage.RESP_REF__RESP_DEF)
-                if (modelElement instanceof RespRef) {
-                    if (resp != null)
-                        resp.eAdapters().remove(this);
-                    resp = ((RespRef) modelElement).getRespDef();
-                    if (resp != null)
-                        resp.eAdapters().add(this);
-                }
-        }
-
-        if (getParent() != null) {
-            ((MapAndPathGraphEditPart) getParent()).notifyChanged(notification);
-            refreshVisuals();
-        }
-    }
-
-    /**
-     * Show direct edit on label on double click, f2 or delay.
-     */
-    public void performRequest(Request request) {
-        if (request.getType() == RequestConstants.REQ_DIRECT_EDIT || request.getType() == RequestConstants.REQ_OPEN) {
-            if (request instanceof DirectEditRequest && !directEditHitTest(((DirectEditRequest) request).getLocation().getCopy()))
-                return;
-            performDirectEdit();
-        }
-    }
-
-    /**
-     * For direct edit, verify location.
-     * 
-     * @param requestLoc
-     * @return
-     */
-    private boolean directEditHitTest(Point requestLoc) {
-        LabelFigure figure = (LabelFigure) getFigure();
-        EditableLabel nameLabel = figure.getLabel();
-        nameLabel.translateToRelative(requestLoc);
-        if (nameLabel.containsPoint(requestLoc))
-            return true;
-        return false;
-    }
-
-    /**
-     * Opens the direct edit manager.
-     *  
-     */
-    protected void performDirectEdit() {
-        if (manager == null) {
-            LabelFigure figure = (LabelFigure) getFigure();
-            EditableLabel nameLabel = figure.getLabel();
-
-            ICellEditorValidator validator = new ICellEditorValidator() {
-                public String isValid(Object value) {
-                    return "";
-                }
-            };
-
-            manager = new ExtendedDirectEditManager(this, TextCellEditor.class, new LabelCellEditorLocator(nameLabel), nameLabel, validator);
-        }
-        manager.show();
-    }
-
-    /**
-     * @param handles
-     *            the name change during an edit
-     */
-    public void handleNameChange(String value) {
-        LabelFigure tableFigure = (LabelFigure) getFigure();
-        EditableLabel label = tableFigure.getLabel();
-        label.setVisible(false);
-        refreshVisuals();
     }
 
     /**
---
+++ src/seg/jUCMNav/views/property/LabelPropertySource.java
@@ -0,0 +1,83 @@
+package seg.jUCMNav.views.property;
+
+import java.util.Vector;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import ucm.map.PathNode;
+import ucm.map.RespRef;
+import urncore.ComponentLabel;
+import urncore.NodeLabel;
+
+/**
+ * Created on 31-May-2005
+ * 
+ * Property source for labels. Extends UCMElementPropertySource to obtain all necessary behaviour to list a label's properties. Includes an EPropertySource to
+ * include all the properties of the labeled element.
+ * 
+ * @author jkealey
+ *  
+ */
+public class LabelPropertySource extends UCMElementPropertySource {
+
+    UCMElementPropertySource referencePS;
+
+    /**
+     * @param obj
+     *            Must be a ComponentLabel or a NodeLabel.
+     */
+    public LabelPropertySource(EObject obj) {
+        super(obj);
+        if (obj instanceof ComponentLabel) {
+            ComponentLabel cl = (ComponentLabel) obj;
+            referencePS = new ComponentPropertySource(cl.getCompRef());
+        } else if (obj instanceof NodeLabel) {
+            NodeLabel nl = (NodeLabel) obj;
+            PathNode pn = nl.getPathNode();
+            if (pn instanceof RespRef)
+                referencePS = new ResponsibilityPropertySource(pn);
+            else
+                referencePS = new UCMElementPropertySource(pn);
+        }
+    }
+
+    /**
+     * Adds all of the labeled element's properties
+     */
+    protected Vector addSpecificProperties() {
+
+        Vector v = new Vector();
+
+        if (referencePS != null) {
+            IPropertyDescriptor pds[] = referencePS.getPropertyDescriptors();
+            for (int i = 0; i < pds.length; i++) {
+                v.add(pds[i]);
+            }
+        }
+        return v;
+    }
+
+    /**
+     * Returns the property value. If is not the label's, delegates.
+     */
+    public Object getPropertyValue(Object id) {
+        Object[] o = (Object[]) id;
+        if ((EClass) o[0] != object.eClass() && referencePS != null)
+            return referencePS.getPropertyValue(id);
+        else
+            return super.getPropertyValue(id);
+    }
+
+    /**
+     * Sets the property value. If is not the label's, delegates.
+     */
+    public void setPropertyValue(Object id, Object value) {
+        Object[] o = (Object[]) id;
+        if ((EClass) o[0] != object.eClass() && referencePS != null)
+            referencePS.setPropertyValue(id, value);
+        else
+            super.setPropertyValue(id, value);
+    }
+}
