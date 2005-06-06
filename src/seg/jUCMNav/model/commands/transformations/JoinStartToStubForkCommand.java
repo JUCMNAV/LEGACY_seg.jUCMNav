c0daa4baa7f35d4260b1cd3017fe55c988323713
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-06-06 16:04:12 +0000

    bug 283 - changed another command that added branches on orforks.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@299 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/model/commands/transformations/JoinStartToStubForkCommand.java
+++ src/seg/jUCMNav/model/commands/transformations/JoinStartToStubForkCommand.java
@@ -2,154 +2,171 @@ package seg.jUCMNav.model.commands.transformations;
 
 import org.eclipse.gef.commands.Command;
 
+import seg.jUCMNav.model.ModelCreationFactory;
 import seg.jUCMNav.model.commands.JUCMNavCommand;
 import ucm.map.NodeConnection;
+import ucm.map.OrFork;
 import ucm.map.PathGraph;
 import ucm.map.PathNode;
 import ucm.map.StartPoint;
+import urncore.Condition;
 
 /**
  * Created 27-05-2005
  * 
- * This command represents the action of a user dragging a StartPoint in a Stub/Fork. This action will erase the StartPoint and add the path as a successor of the
- * stub/fork.
+ * This command represents the action of a user dragging a StartPoint in a Stub/Fork. This action will erase the StartPoint and add the path as a successor of
+ * the stub/fork.
  * 
  * @author Etienne Tremblay
  */
 public class JoinStartToStubForkCommand extends Command implements JUCMNavCommand {
 
-	/**
-	 * <code>oldStartPoint</code>: The start point beeing dragged to the stub.
-	 */
-	private StartPoint oldStartPoint;
+    /**
+     * <code>oldStartPoint</code>: The start point beeing dragged to the stub.
+     */
+    private StartPoint oldStartPoint;
 
-	/**
-	 * <code>stubOrFork</code>: The stub where the start point will get merged.
-	 */
-	private PathNode stubOrFork;
+    /**
+     * <code>stubOrFork</code>: The stub where the start point will get merged.
+     */
+    private PathNode stubOrFork;
 
-	/**
-	 * <code>oldX, oldY</code>: The old coordinates of the start point.
-	 */
-	private int oldX, oldY;
+    /**
+     * <code>oldX, oldY</code>: The old coordinates of the start point.
+     */
+    private int oldX, oldY;
 
-	/**
-	 * <code>ncOldStart</code>: The connection going from the start point initialy.
-	 */
-	private NodeConnection ncOldStart;
+    /**
+     * <code>ncOldStart</code>: The connection going from the start point initialy.
+     */
+    private NodeConnection ncOldStart;
 
-	private PathGraph pg;
+    /**
+     * If connecting to OrFork, add the condition.
+     */
+    private Condition newCondition;
 
-	/**
-	 * @param oldStartPoint
-	 *            The start point beeing dragged to the stub/fork.
-	 * @param stub
-	 *            The stub/fork where the start point will get merged.
-	 */
-	public JoinStartToStubForkCommand(StartPoint oldStartPoint, PathNode stubOrFork) {
-		super();
-		this.oldStartPoint = oldStartPoint;
-		this.stubOrFork = stubOrFork;
-	}
+    private PathGraph pg;
 
-	/**
-	 * Disable the default constructor.
-	 */
-	private JoinStartToStubForkCommand() {
-		super();
-	}
+    /**
+     * @param oldStartPoint
+     *            The start point beeing dragged to the stub/fork.
+     * @param stub
+     *            The stub/fork where the start point will get merged.
+     */
+    public JoinStartToStubForkCommand(StartPoint oldStartPoint, PathNode stubOrFork) {
+        super();
+        this.oldStartPoint = oldStartPoint;
+        this.stubOrFork = stubOrFork;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.commands.Command#canExecute()
-	 */
-	public boolean canExecute() {
-		if (stubOrFork != null && oldStartPoint != null)
-			return true;
-		else
-			return false;
-	}
+    /**
+     * Disable the default constructor.
+     */
+    private JoinStartToStubForkCommand() {
+        super();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.commands.Command#execute()
-	 */
-	public void execute() {
-		// Remember the last position of the startpoint.
-		oldX = oldStartPoint.getX();
-		oldY = oldStartPoint.getY();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.commands.Command#canExecute()
+     */
+    public boolean canExecute() {
+        if (stubOrFork != null && oldStartPoint != null)
+            return true;
+        else
+            return false;
+    }
 
-		pg = oldStartPoint.getPathGraph();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.commands.Command#execute()
+     */
+    public void execute() {
+        // Remember the last position of the startpoint.
+        oldX = oldStartPoint.getX();
+        oldY = oldStartPoint.getY();
 
-		ncOldStart = (NodeConnection) oldStartPoint.getSucc().get(0);
+        pg = oldStartPoint.getPathGraph();
 
-		redo();
-	}
+        ncOldStart = (NodeConnection) oldStartPoint.getSucc().get(0);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.commands.Command#redo()
-	 */
-	public void redo() {
-		testPreConditions();
+        if (stubOrFork instanceof OrFork) {
+            newCondition = (Condition) ModelCreationFactory.getNewObject(pg.getMap().getUcmspec().getUrnspec(), Condition.class);
+        }
+        redo();
+    }
 
-		ncOldStart.setSource(stubOrFork);
-		pg.getPathNodes().remove(oldStartPoint);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.commands.Command#redo()
+     */
+    public void redo() {
+        testPreConditions();
 
-		testPostConditions();
-	}
+        ncOldStart.setSource(stubOrFork);
+        pg.getPathNodes().remove(oldStartPoint);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.commands.Command#undo()
-	 */
-	public void undo() {
-		testPostConditions();
+        if (newCondition!=null)
+            ncOldStart.setCondition(newCondition);
+        
+        testPostConditions();
+    }
 
-		pg.getPathNodes().add(oldStartPoint);
-		ncOldStart.setSource(oldStartPoint);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.commands.Command#undo()
+     */
+    public void undo() {
+        testPostConditions();
 
-		oldStartPoint.setX(oldX);
-		oldStartPoint.setY(oldY);
+        pg.getPathNodes().add(oldStartPoint);
+        ncOldStart.setSource(oldStartPoint);
 
-		testPreConditions();
-	}
+        oldStartPoint.setX(oldX);
+        oldStartPoint.setY(oldY);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPreConditions()
-	 */
-	public void testPreConditions() {
-		assert oldStartPoint != null : "pre old start point";
-		assert stubOrFork != null : "pre stub";
-		assert ncOldStart != null : "pre old node connection";
-		assert pg != null : "pre pathgraph";
+        if (newCondition!=null)
+            ncOldStart.setCondition(null);
+        
+        testPreConditions();
+    }
 
-		assert oldStartPoint.getX() == oldX && oldStartPoint.getY() == oldY : "pre old start position";
-		assert ncOldStart.getSource() == oldStartPoint : "pre connection source is the start point";
-		assert pg.getPathNodes().contains(oldStartPoint) : "pre pathgraph contains the start point";
-		assert pg.getNodeConnections().contains(ncOldStart) : "pre pathgraph contains the connection";
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPreConditions()
+     */
+    public void testPreConditions() {
+        assert oldStartPoint != null : "pre old start point";
+        assert stubOrFork != null : "pre stub";
+        assert ncOldStart != null : "pre old node connection";
+        assert pg != null : "pre pathgraph";
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPostConditions()
-	 */
-	public void testPostConditions() {
-		assert oldStartPoint != null : "post old start point";
-		assert stubOrFork != null : "post stub";
-		assert ncOldStart != null : "post old node connection";
-		assert pg != null : "post pathgraph";
+        assert oldStartPoint.getX() == oldX && oldStartPoint.getY() == oldY : "pre old start position";
+        assert ncOldStart.getSource() == oldStartPoint : "pre connection source is the start point";
+        assert pg.getPathNodes().contains(oldStartPoint) : "pre pathgraph contains the start point";
+        assert pg.getNodeConnections().contains(ncOldStart) : "pre pathgraph contains the connection";
+    }
 
-		assert ncOldStart.getSource() == stubOrFork : "post connection source is the stub";
-		assert !pg.getPathNodes().contains(oldStartPoint) : "post pathgraph doesn't contain the start point";
-		assert pg.getNodeConnections().contains(ncOldStart) : "post pathgraph contains the connection";
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPostConditions()
+     */
+    public void testPostConditions() {
+        assert oldStartPoint != null : "post old start point";
+        assert stubOrFork != null : "post stub";
+        assert ncOldStart != null : "post old node connection";
+        assert pg != null : "post pathgraph";
+
+        assert ncOldStart.getSource() == stubOrFork : "post connection source is the stub";
+        assert !pg.getPathNodes().contains(oldStartPoint) : "post pathgraph doesn't contain the start point";
+        assert pg.getNodeConnections().contains(ncOldStart) : "post pathgraph contains the connection";
+    }
 
 }
