549586ad9de48a87018aa92b06e03f4a47c1fe52
jpdaigle <jpdaigle@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-06-11 22:41:06 +0000

    JP: Fix loops that could occur when dragging start / end points to EmptyPoints or other Start / End points.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@340 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/editpolicies/layout/PathNodeXYLayoutEditPolicy.java
+++ src/seg/jUCMNav/editpolicies/layout/PathNodeXYLayoutEditPolicy.java
@@ -18,8 +18,14 @@ import seg.jUCMNav.model.commands.transformations.JoinEndToStubJoinCommand;
 import seg.jUCMNav.model.commands.transformations.JoinPathsCommand;
 import seg.jUCMNav.model.commands.transformations.JoinStartToStubForkCommand;
 import seg.jUCMNav.model.commands.transformations.MergeStartEndCommand;
+import seg.jUCMNav.model.util.modelexplore.GraphExplorer;
+import seg.jUCMNav.model.util.modelexplore.queries.EndPointFinder;
+import seg.jUCMNav.model.util.modelexplore.queries.StartPointFinder;
+import ucm.map.EndPoint;
 import ucm.map.OrFork;
 import ucm.map.OrJoin;
+import ucm.map.PathNode;
+import ucm.map.StartPoint;
 
 /**
  * Created on 25-May-2005
@@ -68,7 +74,8 @@ public class PathNodeXYLayoutEditPolicy extends XYLayoutEditPolicy {
     /*
      * (non-Javadoc)
      * 
-     * @see org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy#createAddCommand(org.eclipse.gef.EditPart, java.lang.Object)
+     * @see org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy#createAddCommand(org.eclipse.gef.EditPart,
+     *      java.lang.Object)
      */
     protected Command createAddCommand(EditPart child, Object constraint) {
 
@@ -77,18 +84,61 @@ public class PathNodeXYLayoutEditPolicy extends XYLayoutEditPolicy {
         selection.add(getHost());
 
         SelectionHelper sel = new SelectionHelper(selection);
+        PathNode checkNode;
+        Vector vReachable;
+        StartPoint checkStartPoint;
+        EndPoint checkEndPoint;
+        StartPointFinder.RReachableStartPoints rReachableStarts;
+        StartPointFinder.QFindReachableStartPoints qReachableStarts;
+        EndPointFinder.QFindReachableEndPoints qReachableEnds;
+        EndPointFinder.RReachableEndPoints rReachableEnds;
+
         switch (sel.getSelectionType()) {
         case SelectionHelper.ENDPOINT_EMPTYPOINT:
-            OrJoin join = (OrJoin) ModelCreationFactory.getNewObject(sel.getUrnspec(), OrJoin.class);
-            return new JoinPathsCommand(sel.getEmptypoint(), sel.getEndpoint(), join);
+            // JP: use new querying capabilities to make sure the endpoint being dropped is not already reachable from
+            // this EmptyPoint.
+            // If it's reachable, abort because this is an illegal action (destruction of endpoint / creation of loops).
+            checkNode = sel.getEmptypoint();
+            checkEndPoint = sel.getEndpoint();
+            qReachableEnds = new EndPointFinder().new QFindReachableEndPoints(checkNode);
+            rReachableEnds = (EndPointFinder.RReachableEndPoints) GraphExplorer.getInstance().run(qReachableEnds);
+            vReachable = rReachableEnds.getNodes();
+            if (!vReachable.contains(checkEndPoint)) {
+                OrJoin join = (OrJoin) ModelCreationFactory.getNewObject(sel.getUrnspec(), OrJoin.class);
+                return new JoinPathsCommand(sel.getEmptypoint(), sel.getEndpoint(), join);
+            }
+            break; // This is just for style, yo :)
 
         case SelectionHelper.STARTPOINT_EMPTYPOINT:
-            OrFork fork = (OrFork) ModelCreationFactory.getNewObject(sel.getUrnspec(), OrFork.class);
-            return new ForkPathsCommand(sel.getEmptypoint(), sel.getStartpoint(), fork);
+            // JP: use new querying capabilities to make sure the startpoint being dropped is not already reachable from
+            // this node.
+            // If it's reachable, abort because this is an illegal action (destruction of startpoint / creation of
+            // loops).
+            checkNode = sel.getEmptypoint();
+            checkStartPoint = sel.getStartpoint();
+            qReachableStarts = new StartPointFinder().new QFindReachableStartPoints(checkNode);
+            rReachableStarts = (StartPointFinder.RReachableStartPoints) GraphExplorer.getInstance().run(
+                    qReachableStarts);
+            vReachable = rReachableStarts.getNodes();
+            if (!vReachable.contains(checkStartPoint)) {
+                OrFork fork = (OrFork) ModelCreationFactory.getNewObject(sel.getUrnspec(), OrFork.class);
+                return new ForkPathsCommand(sel.getEmptypoint(), sel.getStartpoint(), fork);
+            }
+            break;
 
         case SelectionHelper.STARTPOINT_ENDPOINT:
-            Rectangle cons = getCurrentConstraintFor((GraphicalEditPart) getHost());
-            return new MergeStartEndCommand(sel.getMap(), sel.getStartpoint(), sel.getEndpoint(), cons.x, cons.y);
+            // disable creation of loops by checking that the SP is not already reachable from the EP
+            checkNode = sel.getEndpoint();
+            checkStartPoint = sel.getStartpoint();
+            qReachableStarts = new StartPointFinder().new QFindReachableStartPoints(checkNode);
+            rReachableStarts = (StartPointFinder.RReachableStartPoints) GraphExplorer.getInstance().run(
+                    qReachableStarts);
+            vReachable = rReachableStarts.getNodes();
+            if (!vReachable.contains(checkStartPoint)) {
+                Rectangle cons = getCurrentConstraintFor((GraphicalEditPart) getHost());
+                return new MergeStartEndCommand(sel.getMap(), sel.getStartpoint(), sel.getEndpoint(), cons.x, cons.y);
+            }
+            break;
 
         case SelectionHelper.ENDPOINT_STUB:
             return new JoinEndToStubJoinCommand(sel.getEndpoint(), sel.getStub());
@@ -111,7 +161,8 @@ public class PathNodeXYLayoutEditPolicy extends XYLayoutEditPolicy {
     /*
      * (non-Javadoc)
      * 
-     * @see org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy#createChangeConstraintCommand(org.eclipse.gef.EditPart, java.lang.Object)
+     * @see org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy#createChangeConstraintCommand(org.eclipse.gef.EditPart,
+     *      java.lang.Object)
      */
     protected Command createChangeConstraintCommand(EditPart child, Object constraint) {
         return null;
