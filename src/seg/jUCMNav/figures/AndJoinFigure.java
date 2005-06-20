c0b1d4d52d1a9f9e272d976771b7e2015d7b67b0
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-06-20 14:26:10 +0000

    fixed hover behaviour bugs on and-*
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@395 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/figures/AndJoinFigure.java
+++ src/seg/jUCMNav/figures/AndJoinFigure.java
@@ -11,73 +11,77 @@ import org.eclipse.draw2d.geometry.Transform;
 
 /**
  * Represents an AND-Join in the model. Visually, lines join together on an invisible figure.
+ * 
  * @author jpdaigle, Jordan McManus
  */
 public class AndJoinFigure extends PathNodeFigure implements Rotateable {
 
-   	private Polygon mainFigure;
-	private PointList edges;
+    private Polygon mainFigure;
+    private PointList edges;
 
     public AndJoinFigure() {
-		super();
-	}
-
-	/* (non-Javadoc)
-	 * @see seg.jUCMNav.figures.PathNodeFigure#createFigure()
-	 */
-	protected void createFigure() {
-		mainFigure = new Polygon();
-    	edges = new PointList();
-    	
-    	edges.addPoint(DEFAULT_WIDTH / 2, 0);
-		edges.addPoint(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT);
-		
-		mainFigure.setLineWidth(3);
-		mainFigure.setPoints(edges);
-		add(mainFigure);
-	}
-	public void rotate(double angle) {
-    	Transform t = new Transform();
-    	t.setRotation(angle);
-    	
-    	PointList newEdges = new PointList();
-    	Point center = new Point(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT / 2);
-
-    	for(int i = 0; i<edges.size(); i++) {
-    		Point newPoint = t.getTransformed(new Point(edges.getPoint(i).x - center.x, edges.getPoint(i).y - center.y)); 
-    		newEdges.addPoint(new Point(center.x - newPoint.x, center.y - newPoint.y));
-    	}
-    	
-    	mainFigure.setPoints(newEdges);
+        super();
     }
-	/* (non-Javadoc)
-	 * @see seg.jUCMNav.figures.PathNodeFigure#initAnchor()
-	 */
-	protected void initAnchor() {
-		incomingAnchor = new ChopboxAnchor(this);
-		outgoingAnchor = new ChopboxAnchor(this);
-	}
 
-	protected boolean useLocalCoordinates() {
-		return true;
-	}
-	
-	public void setHover(boolean hover){
-		this.hover = hover;
-		
-		if(selected == false){
-			if(hover)
-				mainFigure.setLineWidth(6);
-			else
-			    mainFigure.setLineWidth(3);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.figures.PathNodeFigure#createFigure()
+     */
+    protected void createFigure() {
+        mainFigure = new Polygon();
+        edges = new PointList();
 
-	
-	/**
-	 * @return Returns the default dimension.
-	 */
-	public static Dimension getDefaultDimension() {
-		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-	}
+        edges.addPoint(DEFAULT_WIDTH / 2, 0);
+        edges.addPoint(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT);
+
+        mainFigure.setLineWidth(3);
+        mainFigure.setPoints(edges);
+        add(mainFigure);
+    }
+
+    public void rotate(double angle) {
+        Transform t = new Transform();
+        t.setRotation(angle);
+
+        PointList newEdges = new PointList();
+        Point center = new Point(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT / 2);
+
+        for (int i = 0; i < edges.size(); i++) {
+            Point newPoint = t.getTransformed(new Point(edges.getPoint(i).x - center.x, edges.getPoint(i).y - center.y));
+            newEdges.addPoint(new Point(center.x - newPoint.x, center.y - newPoint.y));
+        }
+
+        mainFigure.setPoints(newEdges);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.figures.PathNodeFigure#initAnchor()
+     */
+    protected void initAnchor() {
+        incomingAnchor = new ChopboxAnchor(this);
+        outgoingAnchor = new ChopboxAnchor(this);
+    }
+
+    protected boolean useLocalCoordinates() {
+        return true;
+    }
+
+    public void setHover(boolean hover) {
+        this.hover = hover;
+
+        if (hover)
+            mainFigure.setLineWidth(6);
+        else
+            mainFigure.setLineWidth(3);
+    }
+
+    /**
+     * @return Returns the default dimension.
+     */
+    public static Dimension getDefaultDimension() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 }
