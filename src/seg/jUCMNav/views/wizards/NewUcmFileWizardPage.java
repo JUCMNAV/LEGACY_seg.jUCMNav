c816765d1fbbe97b2be920e74ca4d3dedc105fab
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-06-27 10:12:10 +0000

    bug 330 - new file wizard allowed the creation of files without extensions.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@419 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/views/wizards/NewUcmFileWizardPage.java
+++ src/seg/jUCMNav/views/wizards/NewUcmFileWizardPage.java
@@ -14,150 +14,148 @@ import org.eclipse.jface.viewers.*;
 import seg.jUCMNav.JUCMNavPlugin;
 
 /**
- * The "New" wizard page allows setting the container for
- * the new file as well as the file name. The page
- * will only accept file name without the extension OR
+ * The "New" wizard page allows setting the container for the new file as well as the file name. The page will only accept file name without the extension OR
  * with the extension that matches the expected one (jucm).
  */
 
 public class NewUcmFileWizardPage extends WizardPage {
-	private Text containerText;
-	private Text fileText;
-	private ISelection selection;
+    private Text containerText;
+    private Text fileText;
+    private ISelection selection;
 
-	/**
-	 * Constructor for SampleNewWizardPage.
-	 * @param selection
-	 */
-	public NewUcmFileWizardPage(ISelection selection) {
-		super("wizardPage");
-		setTitle("UCM File");
-		setDescription("This wizard creates a new *.jucm file that can be opened with the JUCMNav editor.");
-		this.setImageDescriptor(ImageDescriptor.createFromFile(JUCMNavPlugin.class, "icons/perspectiveIcon.gif"));
-		this.selection = selection;
-	}
+    /**
+     * Constructor for SampleNewWizardPage.
+     * 
+     * @param selection
+     */
+    public NewUcmFileWizardPage(ISelection selection) {
+        super("wizardPage");
+        setTitle("UCM File");
+        setDescription("This wizard creates a new *.jucm file that can be opened with the JUCMNav editor.");
+        this.setImageDescriptor(ImageDescriptor.createFromFile(JUCMNavPlugin.class, "icons/perspectiveIcon.gif"));
+        this.selection = selection;
+    }
 
-	/**
-	 */
-	public void createControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NULL);
-		GridLayout layout = new GridLayout();
-		container.setLayout(layout);
-		layout.numColumns = 3;
-		layout.verticalSpacing = 9;
-		Label label = new Label(container, SWT.NULL);
-		label.setText("&Container:");
+    /**
+     */
+    public void createControl(Composite parent) {
+        Composite container = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        container.setLayout(layout);
+        layout.numColumns = 3;
+        layout.verticalSpacing = 9;
+        Label label = new Label(container, SWT.NULL);
+        label.setText("&Container:");
 
-		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		containerText.setLayoutData(gd);
-		containerText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				dialogChanged();
-			}
-		});
+        containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+        containerText.setLayoutData(gd);
+        containerText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
 
-		Button button = new Button(container, SWT.PUSH);
-		button.setText("Browse...");
-		button.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				handleBrowse();
-			}
-		});
-		label = new Label(container, SWT.NULL);
-		label.setText("&File name:");
+        Button button = new Button(container, SWT.PUSH);
+        button.setText("Browse...");
+        button.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                handleBrowse();
+            }
+        });
+        label = new Label(container, SWT.NULL);
+        label.setText("&File name:");
 
-		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		gd = new GridData(GridData.FILL_HORIZONTAL);
-		fileText.setLayoutData(gd);
-		fileText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				dialogChanged();
-			}
-		});
-		initialize();
-		dialogChanged();
-		setControl(container);
-	}
-	
-	/**
-	 * Tests if the current workbench selection is a suitable
-	 * container to use.
-	 */
-	
-	private void initialize() {
-		if (selection!=null && selection.isEmpty()==false && selection instanceof IStructuredSelection) {
-			IStructuredSelection ssel = (IStructuredSelection)selection;
-			if (ssel.size()>1) return;
-			Object obj = ssel.getFirstElement();
-			if (obj instanceof IResource) {
-				IContainer container;
-				if (obj instanceof IContainer)
-					container = (IContainer)obj;
-				else
-					container = ((IResource)obj).getParent();
-				containerText.setText(container.getFullPath().toString());
-			}
-		}
-		fileText.setText("new_file.jucm");
-	}
-	
-	/**
-	 * Uses the standard container selection dialog to
-	 * choose the new value for the container field.
-	 */
+        fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        fileText.setLayoutData(gd);
+        fileText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
+        initialize();
+        dialogChanged();
+        setControl(container);
+    }
 
-	private void handleBrowse() {
-		ContainerSelectionDialog dialog =
-			new ContainerSelectionDialog(
-				getShell(),
-				ResourcesPlugin.getWorkspace().getRoot(),
-				false,
-				"Select new file container");
-		if (dialog.open() == ContainerSelectionDialog.OK) {
-			Object[] result = dialog.getResult();
-			if (result.length == 1) {
-				containerText.setText(((Path)result[0]).toOSString());
-			}
-		}
-	}
-	
-	/**
-	 * Ensures that both text fields are set.
-	 */
+    /**
+     * Tests if the current workbench selection is a suitable container to use.
+     */
 
-	private void dialogChanged() {
-		String container = getContainerName();
-		String fileName = getFileName();
+    private void initialize() {
+        if (selection != null && selection.isEmpty() == false && selection instanceof IStructuredSelection) {
+            IStructuredSelection ssel = (IStructuredSelection) selection;
+            if (ssel.size() > 1)
+                return;
+            Object obj = ssel.getFirstElement();
+            if (obj instanceof IResource) {
+                IContainer container;
+                if (obj instanceof IContainer)
+                    container = (IContainer) obj;
+                else
+                    container = ((IResource) obj).getParent();
+                containerText.setText(container.getFullPath().toString());
+            }
+        }
+        fileText.setText("new_file.jucm");
+    }
 
-		if (container.length() == 0) {
-			updateStatus("File container must be specified");
-			return;
-		}
-		if (fileName.length() == 0) {
-			updateStatus("File name must be specified");
-			return;
-		}
-		int dotLoc = fileName.lastIndexOf('.');
-		if (dotLoc != -1) {
-			String ext = fileName.substring(dotLoc + 1);
-			if (ext.equalsIgnoreCase("jucm") == false) {
-				updateStatus("File extension must be \"jucm\"");
-				return;
-			}
-		}
-		updateStatus(null);
-	}
+    /**
+     * Uses the standard container selection dialog to choose the new value for the container field.
+     */
 
-	private void updateStatus(String message) {
-		setErrorMessage(message);
-		setPageComplete(message == null);
-	}
+    private void handleBrowse() {
+        ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(), ResourcesPlugin.getWorkspace().getRoot(), false, "Select new file container");
+        if (dialog.open() == ContainerSelectionDialog.OK) {
+            Object[] result = dialog.getResult();
+            if (result.length == 1) {
+                containerText.setText(((Path) result[0]).toOSString());
+            }
+        }
+    }
 
-	public String getContainerName() {
-		return containerText.getText();
-	}
-	public String getFileName() {
-		return fileText.getText();
-	}
+    /**
+     * Ensures that both text fields are set.
+     */
+
+    private void dialogChanged() {
+        String container = getContainerName();
+        String fileName = getFileName();
+
+        if (container.length() == 0) {
+            updateStatus("File container must be specified");
+            return;
+        }
+        if (fileName.length() == 0) {
+            updateStatus("File name must be specified");
+            return;
+        }
+        int dotLoc = fileName.lastIndexOf('.');
+        if (dotLoc != -1) {
+            String ext = fileName.substring(dotLoc + 1);
+            if (ext.equalsIgnoreCase("jucm") == false) {
+                updateStatus("File extension must be \"jucm\"");
+                return;
+            }
+        } else {
+            updateStatus("File extension must be \"jucm\"");
+            return;
+        }
+
+        updateStatus(null);
+    }
+
+    private void updateStatus(String message) {
+        setErrorMessage(message);
+        setPageComplete(message == null);
+    }
+
+    public String getContainerName() {
+        return containerText.getText();
+    }
+
+    public String getFileName() {
+        return fileText.getText();
+    }
 }
