755f2d80e679a86009faca531ac2d1f80da5896f
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-07-14 17:07:25 +0000

    JAVADOC ISSUE AGAIN :)
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@483 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/views/compositeList/CompositeListControl.java
+++ src/seg/jUCMNav/views/compositeList/CompositeListControl.java
@@ -199,7 +199,7 @@ public class CompositeListControl extends Composite implements ISelectionProvide
     }
 
     /**
-     * @see seg.jUCMNav.views.ISelectionInListListener#selectionChanged(Object)
+     * @see seg.jUCMNav.views.compositeList.ISelectionInListListener#selectionChanged(Object)
      */
     public void selectionChanged(Object selected) {
         setSelection(new StructuredSelection(selected));
