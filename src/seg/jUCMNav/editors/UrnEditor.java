fe7798ae96d9a68e970869758cabb10d6834a4d0
jfroy <jfroy@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-08-09 19:31:13 +0000

    Added the editor superclass UrnEditor,  to implement others urn editors used in the MultiPageEditor.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@500 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ src/seg/jUCMNav/editors/UrnEditor.java
@@ -0,0 +1,179 @@
+/**
+ * 
+ */
+package seg.jUCMNav.editors;
+
+import java.util.EventObject;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.eclipse.gef.ui.actions.ActionRegistry;
+import org.eclipse.gef.ui.palette.FlyoutPaletteComposite.FlyoutPreferences;
+import org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette;
+import org.eclipse.gef.ui.parts.SelectionSynchronizer;
+import org.eclipse.ui.IEditorPart;
+
+import seg.jUCMNav.Messages;
+
+/**
+ * @author Jean-François Roy
+ *
+ */
+public abstract class UrnEditor extends GraphicalEditorWithFlyoutPalette {
+
+    /** the parent containing the action registry */
+    protected UCMNavMultiPageEditor parent;
+
+    /** Cache save-request status. */
+    private boolean saveAlreadyRequested;
+
+    /** Create a new UrnEditor instance. */
+    public UrnEditor(UCMNavMultiPageEditor parent) {
+        this.parent = parent;
+        setEditDomain(new DefaultEditDomain(this));
+    }
+
+    /**
+     * Handle events to know when a command was executed. So we can know when we can or cannot save the file.
+     */
+    public void commandStackChanged(EventObject event) {
+        // Note: some actions go directly to this command stack and others go to our parents.
+
+        super.commandStackChanged(event);
+        if (isDirty() && !saveAlreadyRequested) {
+            saveAlreadyRequested = true;
+            firePropertyChange(IEditorPart.PROP_DIRTY);
+        } else {
+            saveAlreadyRequested = false;
+            firePropertyChange(IEditorPart.PROP_DIRTY);
+        }
+    }
+
+    /**
+     * Create all the actions in the action registry.
+     * 
+     * Now done in multipage.
+     * 
+     * DO NOT DELETE: SEE SUPERCLASS
+     * @see org.eclipse.gef.ui.parts.GraphicalEditor#createActions()
+     */
+    protected void createActions() {
+        // now done in MultiPage
+    }
+
+    /**
+     * Used when UcmEditor was the root model element.
+     * 
+     * @deprecated
+     */
+    public void doSave(IProgressMonitor monitor) {
+        System.out.println(Messages.getString("UcmEditor.oldSave")); //$NON-NLS-1$
+    }
+
+    /**
+     * Used when UcmEditor was the root model element.
+     * 
+     * @deprecated
+     */
+    public void doSaveAs() {
+        System.out.println(Messages.getString("UcmEditor.oldSaveAs")); //$NON-NLS-1$
+    }
+
+    /**
+     * Execute a command in the parent's command stack.
+     * 
+     * @param cmd
+     */
+    public void execute(Command cmd) {
+        parent.getDelegatingCommandStack().execute(cmd);
+    }
+    
+    /**
+     * Returns a reference to the multi page action registry.
+     */
+    protected ActionRegistry getActionRegistry() {
+        // one action registry for all editors
+        return parent.getActionRegistry();
+    }
+
+    /**
+     * Overriden to allow external access.
+     * 
+     * @see org.eclipse.gef.ui.parts.GraphicalEditor#getEditDomain()
+     */
+    public DefaultEditDomain getEditDomain() {
+        return super.getEditDomain();
+    }
+
+    /**
+     * Overriden to allow external access.
+     */
+    public GraphicalViewer getGraphicalViewer() {
+        return super.getGraphicalViewer();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette#getPalettePreferences()
+     */
+    protected abstract FlyoutPreferences getPalettePreferences(); 
+
+    /* (non-Javadoc)
+     * @see org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette#getPaletteRoot()
+     */
+    protected abstract PaletteRoot getPaletteRoot();
+
+    /**
+     * Returns this editor's container.
+     * 
+     * @return parent multi-page editor
+     */
+    public UCMNavMultiPageEditor getParent() {
+        return parent;
+    }
+
+    /**
+     * Returns the selection syncronizer object. The synchronizer can be used to sync the selection of 2 or more EditPartViewers.
+     * 
+     * @return the syncrhonizer
+     */
+    protected SelectionSynchronizer getSelectionSynchronizer() {
+        return parent.getSelectionSynchronizer();
+    }
+
+    /**
+     * Return true if the editor contains any changes.
+     * 
+     * @see org.eclipse.ui.ISaveablePart#isDirty()
+     */
+    public boolean isDirty() {
+        return getCommandStack().isDirty();
+    }
+    
+    /**
+     * Save is always allowed, even though it won't happen here.
+     * 
+     * @see org.eclipse.ui.ISaveablePart#isSaveAsAllowed()
+     */
+    public boolean isSaveAsAllowed() {
+        return true;
+    }
+
+    /**
+     * Redo's the command at the top of the parent's redo stack.
+     * 
+     */
+    public void redo() {
+        parent.getDelegatingCommandStack().redo();
+    }
+
+    /**
+     * Undo's the command at the top of the parent's undo stack.
+     * 
+     */
+    public void undo() {
+        parent.getDelegatingCommandStack().undo();
+    }
+}
