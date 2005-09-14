88479d5e3b9359cdc2fa700e73df6b6d32a2e66e
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-09-14 13:43:17 +0000

    bug 395 - localization problem when loading eclipse with -nl option
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@554 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/Messages.java
+++ src/seg/jUCMNav/Messages.java
@@ -5,15 +5,23 @@ import java.util.ResourceBundle;
 
 /**
  * @author Etienne Tremblay
- *  
+ * 
  */
 public class Messages {
     private static final String BUNDLE_NAME = "seg.jUCMNav.messages";//$NON-NLS-1$
 
     private static final String BUNDLE_NAME_FALLBACK = "seg.jUCMNav.messages_en";//$NON-NLS-1$
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME) == null ? ResourceBundle
-            .getBundle(BUNDLE_NAME_FALLBACK) : ResourceBundle.getBundle(BUNDLE_NAME);
+    private static ResourceBundle RESOURCE_BUNDLE;
+    static {
+        try {
+            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME) == null ? ResourceBundle.getBundle(BUNDLE_NAME_FALLBACK) : ResourceBundle
+                    .getBundle(BUNDLE_NAME);
+        } catch (Throwable t) {
+            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME_FALLBACK);
+
+        }
+    }
 
     private Messages() {
     }
