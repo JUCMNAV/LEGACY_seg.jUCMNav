9f47baf8aa0358a80029342b6ec249a70d3fb31b
jfroy <jfroy@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-06-22 00:16:30 +0000

    Fixed problem with IntentionalElementRef size
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@879 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/figures/GrlNodeFigure.java
+++ src/seg/jUCMNav/figures/GrlNodeFigure.java
@@ -172,22 +172,24 @@ public abstract class GrlNodeFigure extends Shape implements LabelElementFigure{
         int minWidth = flowPage.getPreferredSize(width,1).width;
         
         //Loop until we have good dimension for the labels to fit in the node
-        while ((dimEditableLabel.width > (width* Math.floor(height/dimEditableLabel.height)))
-                || width < minWidth){
+        while (dimEditableLabel.width > (width* Math.floor((height-1)/dimEditableLabel.height)) || 
+                width < minWidth){
             height = height + dimEditableLabel.height;
             width = width + 20;
+            System.out.println("In while:minWidth=" + minWidth + " width:" + dimEditableLabel.width + " " + width* Math.floor(height/dimEditableLabel.height));        
+
         }
         if (height < (getDefaultDimension().height - 2*LABEL_PADDING_Y)){
             height = getDefaultDimension().height - 2*LABEL_PADDING_Y;
         }
-        
+        System.out.println("Dimension:width:" + width + " height:" + getDefaultDimension().height);            
         Rectangle r = new Rectangle();
         r.x = LABEL_PADDING_X;
         r.y = LABEL_PADDING_Y;
         r.width = width;
         r.height = height;
         setConstraint(flowPage,r);
-        
+System.out.println("Dimension 2:width:" + width + " height:" + height);        
         setSize(width + 2*LABEL_PADDING_X, height + 2*LABEL_PADDING_Y);
 
     }    
