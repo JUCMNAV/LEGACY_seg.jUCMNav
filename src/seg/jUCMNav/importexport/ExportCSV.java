b136f5c6ba3508277f537500a1cadc688f32a594
jfroy <jfroy@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-10-08 20:53:57 +0000

    Added a CSV export for GRL strategies
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@933 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ src/seg/jUCMNav/importexport/ExportCSV.java
@@ -0,0 +1,164 @@
+/**
+ * 
+ */
+package seg.jUCMNav.importexport;
+
+import grl.Actor;
+import grl.Evaluation;
+import grl.EvaluationStrategy;
+import grl.IntentionalElement;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+
+import seg.jUCMNav.extensionpoints.IURNExport;
+import seg.jUCMNav.strategies.EvaluationStrategyManager;
+import urn.URNspec;
+
+/**
+ * Export the GRL strategies in the cvs (comma-separated values)
+ * 
+ * @author jfroy
+ *
+ */
+public class ExportCSV implements IURNExport {
+	
+    public static final String COMMA = ", "; //$NON-NLS-1$
+    public static final String END_LINE = "\n"; //$NON-NLS-1$
+
+    private FileOutputStream fos = null;
+    private String filename;
+    
+	/* (non-Javadoc)
+	 * @see seg.jUCMNav.extensionpoints.IURNExport#export(urn.URNspec, java.io.FileOutputStream)
+	 */
+	public void export(URNspec urn, FileOutputStream fos)
+			throws InvocationTargetException {
+		// Not Used
+	}
+
+	/* (non-Javadoc)
+	 * @see seg.jUCMNav.extensionpoints.IURNExport#export(urn.URNspec, java.lang.String)
+	 */
+	public void export(URNspec urn, String filename)
+			throws InvocationTargetException {
+        this.filename = filename;
+        try {
+            fos = new FileOutputStream(filename);
+   
+            writeHeader(urn);
+            writeStrategies(urn);
+        } catch (Exception e) {
+            throw new InvocationTargetException(e);
+        } finally {
+            // close the stream
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+	}
+
+    /**
+     * Write the string to the file output stream.
+     * 
+     * @param s
+     *            the string to write
+     * @throws IOException
+     */
+    public void write(String s) throws IOException {
+        if (s != null && s.length() > 0) {
+            fos.write(s.getBytes());
+        }
+    }
+    
+    /**
+     * 
+     * @param urn
+     * 			URNspec
+     * @throws IOException
+     */
+    private void writeHeader(URNspec urn) throws IOException{
+        write("Name, Description, Author");//$NON-NLS-1$    
+        //Write the actors name in the header
+        for (Iterator iter = urn.getGrlspec().getActors().iterator(); iter.hasNext();) {
+            Actor actor = (Actor)iter.next();
+        	write(COMMA);
+        	write(actor.getName());
+        	write(" (A)"); //$NON-NLS-1$
+        }
+ 
+        //Write the intentional element name in the header
+        for (Iterator iter = urn.getGrlspec().getIntElements().iterator(); iter.hasNext();) {
+            IntentionalElement element = (IntentionalElement) iter.next();
+        	write(COMMA);
+        	write(element.getName());
+        }
+        write(END_LINE);        
+    }
+    
+    /**
+     * Writes the strategies information.
+     * 
+     * @param urn
+     *            urnspec
+     * @throws IOException
+     */
+    private void writeStrategies(URNspec urn) throws IOException {
+    	for (Iterator iter = urn.getGrlspec().getStrategies().iterator(); iter.hasNext();){
+            EvaluationStrategy strategy = (EvaluationStrategy)iter.next();
+           
+            //Name
+            write(strategy.getName());
+            
+            //Description
+            write(COMMA);
+            write(strategy.getDescription());
+            
+            //Author
+            write(COMMA);
+            write(strategy.getAuthor());         
+            writeEvaluations(strategy);
+            write(END_LINE); //$NON-NLS-1$
+        }
+    }
+    
+    /**
+     * Writes the information about evaluations for a grl stategy.
+     * 
+     * @param strategy
+     *            EvaluationStrategy
+     * @throws IOException
+     */
+    private void writeEvaluations(EvaluationStrategy strategy) throws IOException {
+        EvaluationStrategyManager.getInstance(false).setStrategy(strategy);
+        EvaluationStrategyManager.getInstance(false).calculateEvaluation();
+
+        //Write evaluation for actors
+        for (Iterator iter = strategy.getGrlspec().getActors().iterator(); iter.hasNext();) {
+            Actor actor = (Actor)iter.next();
+           
+            int evaluation = EvaluationStrategyManager.getInstance(false).getActorEvaluation(actor);
+            
+            write(COMMA + evaluation);
+        }
+
+        //Write evaluation for intentional elements
+        for (Iterator iter = strategy.getGrlspec().getIntElements().iterator(); iter.hasNext();) {
+            IntentionalElement element = (IntentionalElement) iter.next();
+            
+            Evaluation evaluation = EvaluationStrategyManager.getInstance(false).getEvaluationObject(element);
+            
+            if(evaluation.getStrategies() != null) {
+               	write(COMMA + evaluation.getEvaluation());
+            } else {
+            	write(COMMA + evaluation.getEvaluation() + "*");
+            }
+        }
+    }
+}
