532791f6abe0a288d6b1473d831223e5d3070278
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2007-01-10 18:24:48 +0000

    scenarios: refactored MakeWellFormedCommand to use a SimplifyForksAndJoinsCommand
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1070 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/model/commands/transformations/MakeWellFormedCommand.java
+++ src/seg/jUCMNav/model/commands/transformations/MakeWellFormedCommand.java
@@ -1,6 +1,5 @@
 package seg.jUCMNav.model.commands.transformations;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Vector;
@@ -8,7 +7,6 @@ import java.util.Vector;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.gef.commands.CompoundCommand;
 
-import seg.jUCMNav.model.commands.delete.DeleteNodeConnectionCommand;
 import seg.jUCMNav.model.commands.delete.DeletePathNodeCommand;
 import seg.jUCMNav.model.util.modelexplore.GraphExplorer;
 import seg.jUCMNav.model.util.modelexplore.queries.ReachableNodeFinder;
@@ -32,6 +30,7 @@ import urncore.IURNContainerRef;
  * I have a feeling that it might not work well on very complex ucms because we change the model between the time we look for intermediate flow points and the
  * time we actually try to fix them. (because we fixed the the graph for a previous flowpoint)
  * 
+ * This command is not reversible! 
  * @author jkealey
  * 
  */
@@ -74,7 +73,7 @@ public class MakeWellFormedCommand extends CompoundCommand {
 	}
 
 	protected void modify(UCMmap map) {
-		Vector andjoins = findAndJoins(map, false);
+		Vector andjoins = SimplifyForksAndJoinsCommand.findAndJoins(map, false);
 		HashSet completedFlowPoints = new HashSet();
 
 		// For each and-join,
@@ -85,9 +84,9 @@ public class MakeWellFormedCommand extends CompoundCommand {
 			if (andjoin.getDiagram() == null)
 				continue;
 
-			Vector[] flowPoints = findUpstreamFlowPoints(andjoin);
+			Vector[] flowPoints = SimplifyForksAndJoinsCommand.findUpstreamFlowPoints(andjoin);
 
-			PathNode commonFlowPoint = findCommonSource(andjoin, flowPoints);
+			PathNode commonFlowPoint = SimplifyForksAndJoinsCommand.findCommonSource(flowPoints);
 
 			if (commonFlowPoint != null) {
 				// b. If the fork is the same for all branches, graph is well-nested and continues. Marks fork as complete.
@@ -99,133 +98,10 @@ public class MakeWellFormedCommand extends CompoundCommand {
 			}
 		}
 		
-		
-		simplifyComplexAndJoins(map);
-		simplifyComplexAndForks(map);
+		cs.execute(new SimplifyForksAndJoinsCommand(map));
 	}
 
-	private void simplifyComplexAndJoins(UCMmap map) {
-		Vector complexAndJoins = findAndJoins(map, true);
-		for (Iterator iter = complexAndJoins.iterator(); iter.hasNext();) {
-			AndJoin andjoin = (AndJoin) iter.next();
-
-			Vector[] flowPoints = findUpstreamFlowPoints(andjoin);
-
-			// if they all share the same source, don't need to do anything
-			if (findCommonSource(andjoin, flowPoints) != null)
-				continue;
-			
-			HashMap hmForkToNodeConnection = new HashMap();
-			
-			for (int i=0;i<flowPoints.length;i++)
-			{
-				Vector v = null;
-				if (!hmForkToNodeConnection.containsKey(flowPoints[i].firstElement())) {
-					v = new Vector();
-				} else {
-					v = (Vector) hmForkToNodeConnection.get(flowPoints[i].firstElement());
-				}
-				v.add(andjoin.getPred().get(i));
-				hmForkToNodeConnection.put(flowPoints[i].firstElement(), v);
-			}
-
-			// we know there is more than one
-			assert hmForkToNodeConnection.values().size()>=2;
-			for (Iterator iterator = hmForkToNodeConnection.values().iterator(); iterator.hasNext();) {
-				Vector v = (Vector) iterator.next();
-				
-				// if only one, skip. otherwise merge others into one
-				if (v.size()>1) {
-		
-					mergeInPathsIntoJoin(andjoin, v);
-				}
-				
-			}
-			
-		}
-	}
-
-	private void mergeInPathsIntoJoin(AndJoin andjoin, Vector v) {
-		NodeConnection nc = (NodeConnection)v.get(1);
-		// will detach it
-		DeleteNodeConnectionCommand cmd = new DeleteNodeConnectionCommand(nc, null);
-		cs.execute(cmd);
-		DividePathCommand cmd2 = new DividePathCommand((EndPoint)nc.getTarget(), (NodeConnection)v.get(0), andjoin.getX()-10, andjoin.getY()-10, false);
-		cs.execute(cmd2);
-		AndJoin newAndJoin = (AndJoin) cmd2.getNewNode();
-		
-		for (int i=2;i<v.size();i++) {
-			nc = (NodeConnection)v.get(i);
-			// will detach it
-			cmd = new DeleteNodeConnectionCommand(nc, null);
-			cs.execute(cmd);
-			
-			AttachBranchCommand cmd3 = new AttachBranchCommand((EndPoint)nc.getTarget(), newAndJoin);
-			cs.execute(cmd3);
-			
-		}
-	}
-	private void simplifyComplexAndForks(UCMmap map) {
-		Vector complexAndForks = findAndForks(map, true);
-		for (Iterator iter = complexAndForks.iterator(); iter.hasNext();) {
-			AndFork andfork = (AndFork) iter.next();
-
-			Vector[] flowPoints = findDownstreamFlowPoints(andfork);
-
-			// if they all share the same source, don't need to do anything
-			if (findCommonSource(andfork, flowPoints) != null)
-				continue;
-			
-			HashMap hmForkToNodeConnection = new HashMap();
-			
-			for (int i=0;i<flowPoints.length;i++)
-			{
-				Vector v = null;
-				if (!hmForkToNodeConnection.containsKey(flowPoints[i].firstElement())) {
-					v = new Vector();
-				} else {
-					v = (Vector) hmForkToNodeConnection.get(flowPoints[i].firstElement());
-				}
-				v.add(andfork.getSucc().get(i));
-				hmForkToNodeConnection.put(flowPoints[i].firstElement(), v);
-			}
-
-			// we know there is more than one
-			assert hmForkToNodeConnection.values().size()>=2;
-			for (Iterator iterator = hmForkToNodeConnection.values().iterator(); iterator.hasNext();) {
-				Vector v = (Vector) iterator.next();
-				
-				// if only one, skip. otherwise merge others into one
-				if (v.size()>1) {
-		
-					mergeInPathsIntoFork(andfork, v);
-				}
-				
-			}
-			
-		}
-	}
-
-	private void mergeInPathsIntoFork(AndFork andfork, Vector v) {
-		NodeConnection nc = (NodeConnection)v.get(1);
-		// will detach it
-		DeleteNodeConnectionCommand cmd = new DeleteNodeConnectionCommand(nc, null);
-		cs.execute(cmd);
-		DividePathCommand cmd2 = new DividePathCommand((StartPoint)nc.getSource(), (NodeConnection)v.get(0), andfork.getX()+10, andfork.getY()+10, false);
-		cs.execute(cmd2);
-		AndFork newAndFork = (AndFork) cmd2.getNewNode();
-		
-		for (int i=2;i<v.size();i++) {
-			nc = (NodeConnection)v.get(i);
-			// will detach it
-			cmd = new DeleteNodeConnectionCommand(nc, null);
-			cs.execute(cmd);
-			
-			AttachBranchCommand cmd3 = new AttachBranchCommand((StartPoint)nc.getSource(), newAndFork);
-			cs.execute(cmd3);
-			
-		}
-	}
+	
 
 	
 	private Vector findUpstreamIntermediateFlowPoints(AndJoin andjoin, Vector[] flowPoints, PathNode mostRecentForkCommonToAllBranches) {
@@ -258,102 +134,9 @@ public class MakeWellFormedCommand extends CompoundCommand {
 		return mostRecentForkCommonToAllBranches;
 	}
 
-	private PathNode findCommonSource(AndJoin andjoin, Vector[] flowPoints) {
-		PathNode commonFork = (PathNode) flowPoints[0].firstElement();
-		for (int i = 1; i < andjoin.getPred().size(); i++) {
-			if (flowPoints[i].firstElement() != commonFork)
-				commonFork = null;
-		}
-		return commonFork;
-	}
 	
-	private PathNode findCommonSource(AndFork andfork, Vector[] flowPoints) {
-		PathNode commonJoin = (PathNode) flowPoints[0].firstElement();
-		for (int i = 1; i < andfork.getSucc().size(); i++) {
-			if (flowPoints[i].firstElement() != commonJoin)
-				commonJoin = null;
-		}
-		return commonJoin;
-	}
-
 	
-	private Vector[] findUpstreamFlowPoints(AndJoin andjoin) {
-		Vector[] flowPoints = new Vector[andjoin.getPred().size()];
-
-		for (int i = 0; i < andjoin.getPred().size(); i++) {
-			NodeConnection nc = (NodeConnection) andjoin.getPred().get(i);
-
-			HashSet exclusions = new HashSet();
-			exclusions.addAll(andjoin.getPred());
-			exclusions.remove(nc);
-
-			flowPoints[i] = new Vector();
-			// 1) look at all incoming paths and go in inverse path direction back up to start point
-			QFindReachableNodes qReachableNodes = new ReachableNodeFinder().new QFindReachableNodes(andjoin, exclusions, QFindReachableNodes.DIRECTION_REVERSE);
-			ReachableNodeFinder.RReachableNodes rReachableNodes = (ReachableNodeFinder.RReachableNodes) GraphExplorer.getInstance().run(qReachableNodes);
-			Vector vReachable = rReachableNodes.getNodes();
-
-			// keep only forks and start point.
-			for (Iterator iterator = vReachable.iterator(); iterator.hasNext();) {
-				PathNode element = (PathNode) iterator.next();
-				if (element instanceof StartPoint || element instanceof AndFork || element instanceof AndJoin) {
-					if (element != andjoin)
-						flowPoints[i].add(element);
-				}
-			}
-
-		}
-		return flowPoints;
-	}
-
-	private Vector[] findDownstreamFlowPoints(AndFork andfork) {
-		Vector[] flowPoints = new Vector[andfork.getSucc().size()];
-
-		for (int i = 0; i < andfork.getSucc().size(); i++) {
-			NodeConnection nc = (NodeConnection) andfork.getSucc().get(i);
-
-			HashSet exclusions = new HashSet();
-			exclusions.addAll(andfork.getSucc());
-			exclusions.remove(nc);
-
-			flowPoints[i] = new Vector();
-			// 1) look at all incoming paths and go in forward path direction down to end piont
-			QFindReachableNodes qReachableNodes = new ReachableNodeFinder().new QFindReachableNodes(andfork, exclusions, QFindReachableNodes.DIRECTION_FORWARD);
-			ReachableNodeFinder.RReachableNodes rReachableNodes = (ReachableNodeFinder.RReachableNodes) GraphExplorer.getInstance().run(qReachableNodes);
-			Vector vReachable = rReachableNodes.getNodes();
-
-			// keep only forks and start point.
-			for (Iterator iterator = vReachable.iterator(); iterator.hasNext();) {
-				PathNode element = (PathNode) iterator.next();
-				if (element instanceof StartPoint || element instanceof AndFork || element instanceof AndJoin) {
-					if (element != andfork)
-						flowPoints[i].add(element);
-				}
-			}
-
-		}
-		return flowPoints;
-	}
 	
-	private Vector findAndJoins(UCMmap map, boolean keepOnlyMoreThanTwoIn) {
-		Vector andjoins = new Vector();
-		for (Iterator iter = map.getNodes().iterator(); iter.hasNext();) {
-			PathNode pn = (PathNode) iter.next();
-			if (pn instanceof AndJoin && (!keepOnlyMoreThanTwoIn || pn.getPred().size()>2))
-				andjoins.add(pn);
-		}
-		return andjoins;
-	}
-	private Vector findAndForks(UCMmap map, boolean keepOnlyMoreThanTwoOut) {
-		Vector andforks = new Vector();
-		for (Iterator iter = map.getNodes().iterator(); iter.hasNext();) {
-			PathNode pn = (PathNode) iter.next();
-			if (pn instanceof AndFork && (!keepOnlyMoreThanTwoOut || pn.getSucc().size()>2))
-				andforks.add(pn);
-		}
-		return andforks;
-	}
-
 	
 	private void fixUpstreamFlowPoints(UCMmap map, AndJoin andjoin, Vector toBeFixed, HashSet completedFlowPoints) {
 		for (Iterator iterator = toBeFixed.iterator(); iterator.hasNext();) {
