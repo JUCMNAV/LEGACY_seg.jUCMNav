d4b278e2d3ea3f8229105a62bb1bd7f06b9e9192
jack <jack@1d5a7336-c65e-6947-a78c-d4236001c669>
2007-03-20 10:50:30 +0000

    one file got lost behind...
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1162 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ src/seg/jUCMNav/actions/performance/ManageResourcesAction.java
@@ -0,0 +1,80 @@
+package seg.jUCMNav.actions.performance;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+
+import seg.jUCMNav.JUCMNavPlugin;
+import seg.jUCMNav.actions.SelectionHelper;
+import seg.jUCMNav.actions.URNSelectionAction;
+import seg.jUCMNav.views.wizards.performance.ManageResources;
+import ucm.map.ComponentRef;
+import ucm.map.RespRef;
+import urn.URNspec;
+import urncore.Component;
+import urncore.Responsibility;
+
+/**
+ * Opens the resource editor
+ * 
+ * @author jack
+ */
+public class ManageResourcesAction extends URNSelectionAction {
+
+	public static final String MANAGERESOURCESACTION = "seg.jUCMNav.actions.performance.ManageResourcesAction";
+
+	private EObject obj;
+	private URNspec urn;
+
+	/**
+	 * Opens the resource editor.
+	 * 
+	 * @param part
+	 *            the UCMNavMultiPageEditor
+	 */
+	public ManageResourcesAction(IWorkbenchPart part) {
+		super(part);
+		setId(MANAGERESOURCESACTION);
+		setImageDescriptor(ImageDescriptor.createFromFile(JUCMNavPlugin.class, "icons/Resource.gif")); //$NON-NLS-1$
+	}
+
+	/**
+	 * True if we've selected something with resource.
+	 */
+	protected boolean calculateEnabled() {
+		boolean enable = false;
+
+		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
+		obj = sel.getURNmodelElement();
+
+		if (obj instanceof ComponentRef) {
+		    this.urn = ((Component)((ComponentRef)obj).getContDef()).getUrndefinition().getUrnspec();
+		    enable = true;
+		}
+		if (obj instanceof RespRef) {
+		    this.urn = ((Responsibility)((RespRef)obj).getRespDef()).getUrndefinition().getUrnspec();
+		    enable = true;
+		}
+
+		return enable;
+	}
+
+	/**
+	 * Opens the resource editor.
+	 * 
+	 * @see seg.jUCMNav.views.wizards.performance.ManageResources
+	 * @see org.eclipse.jface.action.IAction#run()
+	 */
+	public void run() {
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		ManageResources wizard = new ManageResources(urn);
+		wizard.init(PlatformUI.getWorkbench(), obj);
+		WizardDialog dialog = new WizardDialog(shell, wizard);
+		dialog.open();
+
+	}
+
+}
