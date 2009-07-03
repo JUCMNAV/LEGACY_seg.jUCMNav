4b3f59c2620dd5b98b211e9eaf43bec6f7145537
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2009-07-03 20:31:45 +0000

    bug 466: refactored paste from action into command.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1534 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/actions/cutcopypaste/PasteAction.java
+++ src/seg/jUCMNav/actions/cutcopypaste/PasteAction.java
@@ -1,10 +1,7 @@
 package seg.jUCMNav.actions.cutcopypaste;
 
-import java.util.Iterator;
-import java.util.List;
-
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.internal.GEFMessages;
 import org.eclipse.ui.ISharedImages;
@@ -14,29 +11,10 @@ import org.eclipse.ui.actions.ActionFactory;
 
 import seg.jUCMNav.actions.SelectionHelper;
 import seg.jUCMNav.actions.URNSelectionAction;
-import seg.jUCMNav.model.ModelCreationFactory;
-import seg.jUCMNav.model.commands.transformations.DividePathCommand;
-import seg.jUCMNav.model.commands.transformations.ReplaceEmptyPointCommand;
-import seg.jUCMNav.model.commands.transformations.SplitLinkCommand;
-import seg.jUCMNav.model.util.Clipboard;
-import seg.jUCMNav.model.util.URNElementFinder;
-import ucm.map.AndFork;
-import ucm.map.AndJoin;
-import ucm.map.Connect;
-import ucm.map.DirectionArrow;
-import ucm.map.EmptyPoint;
-import ucm.map.EndPoint;
+import seg.jUCMNav.model.commands.cutcopypaste.PasteCommand;
 import ucm.map.NodeConnection;
-import ucm.map.OrFork;
-import ucm.map.OrJoin;
-import ucm.map.PathNode;
-import ucm.map.RespRef;
-import ucm.map.StartPoint;
-import ucm.map.Stub;
-import ucm.map.Timer;
+import ucm.map.UCMmap;
 import urn.URNspec;
-import urncore.Condition;
-import urncore.Responsibility;
 
 public class PasteAction extends URNSelectionAction
 {
@@ -50,190 +28,20 @@ public class PasteAction extends URNSelectionAction
 		setImageDescriptor(sharedImages.getImageDescriptor(ISharedImages.IMG_TOOL_PASTE));
 		setDisabledImageDescriptor(sharedImages.getImageDescriptor(ISharedImages.IMG_TOOL_PASTE_DISABLED));
 	}
-
-	protected boolean calculateEnabled()
-	{
-		boolean found = getSelectedInsertionPoint() != null;
-
-		if (found)
-		{
-			// we've selected something that is copiable. 
-			found = getFirstResponsibility() != null || getFirstPathNode()!=null;
-			return found;
-		}
-		else
-			return false;
-	}
-
+	
 	protected Command getCommand()
 	{
 		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
-		
-		
 		EObject ep = getSelectedInsertionPoint();
+		UCMmap targetMap = sel.getMap();
+		URNspec targetUrn = sel.getUrnspec();
+		Point nodeConnectionMiddle = null;
+		if (ep instanceof NodeConnection) 
+			nodeConnectionMiddle = sel.getNodeconnectionMiddle();
 		
-		if (ep==null)
-			return null; 
-		else
-		{
-			Responsibility def = getFirstResponsibility();
-			
-			
-			// this must only be executed when actually doing the initial insert. otherwise create bogus items that are not used
-			ModelCreationFactory factory;
-			PathNode newPathNode=null;
-			PathNode oldPn=null;
-			if (def!=null) {
-				factory = new ModelCreationFactory(sel.getUrnspec(), RespRef.class, def);
-				newPathNode= (RespRef)factory.getNewObject();
-			} else
-			{
-				oldPn = getFirstPathNode();
-				//if (oldPn!=null && oldPn.getClass().getInterfaces().length>0) {
-					//factory = new ModelCreationFactory(sel.getUrnspec(), oldPn.getClass().getInterfaces()[0], oldPn);
-					//newPathNode= (PathNode) factory.getNewObject();
-				//}
-				if (oldPn!=null)
-				{
-					newPathNode = (PathNode) EcoreUtil.copy(oldPn); 
-				}
-			}
-			
-			if (newPathNode!=null) {
-				
-				// ensures that created branches are at the right place. 
-				if (ep instanceof PathNode) {
-					newPathNode.setX(((PathNode) ep).getX());
-					newPathNode.setY(((PathNode) ep).getY());
-				} else if (ep instanceof NodeConnection)
-				{
-					newPathNode.setX(sel.getNodeconnectionMiddle().x);
-					newPathNode.setY(sel.getNodeconnectionMiddle().y);
-				}
-
-				if (newPathNode instanceof AndFork || newPathNode instanceof OrFork || newPathNode instanceof AndJoin || newPathNode instanceof OrJoin || newPathNode instanceof Stub || newPathNode instanceof Timer) 
-				{
-					DividePathCommand cmd = null;
-					if (ep instanceof EmptyPoint) {
-						cmd = new DividePathCommand(newPathNode, (EmptyPoint)ep);
-					}
-					else if (ep instanceof DirectionArrow)
-						cmd = new DividePathCommand(newPathNode, (DirectionArrow)ep);
-					else if (ep instanceof NodeConnection)
-						cmd = new DividePathCommand(newPathNode, sel.getNodeconnection(), sel.getNodeconnectionMiddle().x, sel.getNodeconnectionMiddle().y);
-					
-					if (cmd!=null && oldPn!=null)
-						cmd.cloneBranchesFrom(oldPn, sel.getMap());
-					return cmd;
-				} 
-				else 
-				{
-					Condition cond= null;
-					if (oldPn.getSucc().size()>0)
-					{
-						NodeConnection nc = (NodeConnection)oldPn.getSucc().get(0);
-						cond = nc.getCondition();
-					}
-					
-					if (ep instanceof PathNode)
-						return new ReplaceEmptyPointCommand((PathNode) ep, newPathNode, cond );
-					else if (ep instanceof NodeConnection)
-			            return new SplitLinkCommand(sel.getMap(), newPathNode, sel.getNodeconnection(), sel.getNodeconnectionMiddle().x, sel.getNodeconnectionMiddle().y, cond) ;
-				}
-			}
-
-		}
-		
-		return null;
-	}
-
-	protected PathNode getFirstPathNode()
-	{
-		Clipboard clip = Clipboard.getInstance();
-		List content = clip.getSelectedIds();
-		URNspec oldurn = clip.getOriginalURNspec();
-		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
-		URNspec urn = sel.getUrnspec();
-
-		if (content != null && oldurn != null && urn != null)
-		{
-			for (Iterator iterator = content.iterator(); iterator.hasNext();)
-			{
-				String id = iterator.next().toString();
-
-				// search for the old id in the new model. 
-				Object obj = URNElementFinder.find(urn, id); 
-
-				if (obj != null) // found it
-				{
-					if (IsPastablePathNode(obj))
-						return (PathNode) obj; // pn in current urn
-				}
-				else 				// was deleted since.
-				{
-					obj = URNElementFinder.find(oldurn, id);
-					if (IsPastablePathNode(obj))
-					{
-						PathNode oldPn =(PathNode) obj;
-						return oldPn; // pn from old urn
-					}
-				}
-			}
-		}
-		return null;
+		return new PasteCommand(ep, targetUrn, targetMap, nodeConnectionMiddle);
 	}
 	
-	protected boolean IsPastablePathNode(Object obj)
-	{
-		return obj instanceof PathNode && !(obj instanceof EndPoint) && !(obj instanceof StartPoint) && !(obj instanceof Connect);   
-	}
-	
-	/***
-	 * Special case for responsibilities.
-	 * 
-	 * @return the first Responsibility in our 
-	 */
-	protected Responsibility getFirstResponsibility()
-	{
-		Clipboard clip = Clipboard.getInstance();
-		List content = clip.getSelectedIds();
-		URNspec oldurn = clip.getOriginalURNspec();
-		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
-		URNspec urn = sel.getUrnspec();
-
-		if (content != null && oldurn != null && urn != null)
-		{
-			for (Iterator iterator = content.iterator(); iterator.hasNext();)
-			{
-				String id = iterator.next().toString();
-
-				// search for the old id in the new model. 
-				Object obj = URNElementFinder.find(urn, id); 
-
-				if (obj != null) // found it
-				{
-					if (obj instanceof RespRef)
-						return ((RespRef) obj).getRespDef();
-				}
-				
-				// was deleted since.
-				obj = URNElementFinder.find(oldurn, id);
-				if (obj instanceof RespRef)
-				{
-					String oldDefinition = ((RespRef) obj).getRespDef().getId();
-					// this one is faster... searching for the definition.
-					Responsibility resp = URNElementFinder.findResponsibility(urn, oldDefinition);
-					if (resp==null) {
-						resp = URNElementFinder.findResponsibility(oldurn, oldDefinition);
-						resp = (Responsibility) EcoreUtil.copy(resp); // clone it because it is used to create a new element. 
-					}
-					return resp;
-				}
-			}
-		}
-		return null;
-	}
-
 	protected EObject getSelectedInsertionPoint()
 	{
 		if (getSelectedObjects().size() == 1)
--- src/seg/jUCMNav/model/commands/cutcopypaste/PasteCommand.java
+++ src/seg/jUCMNav/model/commands/cutcopypaste/PasteCommand.java
@@ -1,27 +1,237 @@
 package seg.jUCMNav.model.commands.cutcopypaste;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CompoundCommand;
 
-public class PasteCommand extends Command
+import seg.jUCMNav.model.ModelCreationFactory;
+import seg.jUCMNav.model.commands.transformations.DividePathCommand;
+import seg.jUCMNav.model.commands.transformations.ReplaceEmptyPointCommand;
+import seg.jUCMNav.model.commands.transformations.SplitLinkCommand;
+import seg.jUCMNav.model.util.Clipboard;
+import seg.jUCMNav.model.util.URNElementFinder;
+import ucm.map.AndFork;
+import ucm.map.AndJoin;
+import ucm.map.Connect;
+import ucm.map.DirectionArrow;
+import ucm.map.EmptyPoint;
+import ucm.map.EndPoint;
+import ucm.map.NodeConnection;
+import ucm.map.OrFork;
+import ucm.map.OrJoin;
+import ucm.map.PathNode;
+import ucm.map.RespRef;
+import ucm.map.StartPoint;
+import ucm.map.Stub;
+import ucm.map.Timer;
+import ucm.map.UCMmap;
+import urn.URNspec;
+import urncore.Condition;
+import urncore.Responsibility;
+
+public class PasteCommand extends CompoundCommand
 {
-
-	public PasteCommand(EObject target)
-	{
+	protected EObject insertionPoint; 
+	protected UCMmap targetMap; 
+	protected URNspec targetUrn;
+	protected Point nodeConnectionMiddle; 
 	
-	}
-	public void redo()
+	public PasteCommand(EObject insertionPoint, URNspec targetUrn, UCMmap targetMap, Point nodeConnectionMiddle)
 	{
+		this.insertionPoint = insertionPoint;
+		this.targetUrn=targetUrn;
+		this.targetMap=targetMap;
+		this.nodeConnectionMiddle = nodeConnectionMiddle;
+	}
+
+	public boolean canExecute()
+	{
+		boolean found = insertionPoint != null;
+
+		if (found)
+		{
+			// we've selected something that is copiable. 
+			found = getFirstResponsibility() != null || getFirstPathNode()!=null;
+			return found;
+		}
+		else
+			return false;
 	}
 	
 	public void execute()
 	{
-		redo();
+		build();
+		super.execute();
 	}
 	
 	
-	public void undo()
+	protected PathNode getFirstPathNode()
 	{
+		Clipboard clip = Clipboard.getInstance();
+		List content = clip.getSelectedIds();
+		URNspec oldurn = clip.getOriginalURNspec();
+		//SelectionHelper sel = new SelectionHelper(getSelectedObjects());
+		//URNspec urn = sel.getUrnspec();
+
+		if (content != null && oldurn != null && targetUrn != null)
+		{
+			for (Iterator iterator = content.iterator(); iterator.hasNext();)
+			{
+				String id = iterator.next().toString();
+
+				// search for the old id in the new model. 
+				Object obj = URNElementFinder.find(targetUrn, id); 
+
+				if (obj != null) // found it
+				{
+					if (IsPastablePathNode(obj))
+						return (PathNode) obj; // pn in current urn
+				}
+				else 				// was deleted since.
+				{
+					obj = URNElementFinder.find(oldurn, id);
+					if (IsPastablePathNode(obj))
+					{
+						PathNode oldPn =(PathNode) obj;
+						return oldPn; // pn from old urn
+					}
+				}
+			}
+		}
+		return null;
 	}
 	
+	protected boolean IsPastablePathNode(Object obj)
+	{
+		return obj instanceof PathNode && !(obj instanceof EndPoint) && !(obj instanceof StartPoint) && !(obj instanceof Connect);   
+	}
+	
+	/***
+	 * Special case for responsibilities.
+	 * 
+	 * @return the first Responsibility in our 
+	 */
+	protected Responsibility getFirstResponsibility()
+	{
+		Clipboard clip = Clipboard.getInstance();
+		List content = clip.getSelectedIds();
+		URNspec oldurn = clip.getOriginalURNspec();
+
+
+		if (content != null && oldurn != null && targetUrn != null)
+		{
+			for (Iterator iterator = content.iterator(); iterator.hasNext();)
+			{
+				String id = iterator.next().toString();
+
+				// search for the old id in the new model. 
+				Object obj = URNElementFinder.find(targetUrn, id); 
+
+				if (obj != null) // found it
+				{
+					if (obj instanceof RespRef)
+						return ((RespRef) obj).getRespDef();
+				}
+				
+				// was deleted since.
+				obj = URNElementFinder.find(oldurn, id);
+				if (obj instanceof RespRef)
+				{
+					String oldDefinition = ((RespRef) obj).getRespDef().getId();
+					// this one is faster... searching for the definition.
+					Responsibility resp = URNElementFinder.findResponsibility(targetUrn, oldDefinition);
+					if (resp==null) {
+						resp = URNElementFinder.findResponsibility(oldurn, oldDefinition);
+						resp = (Responsibility) EcoreUtil.copy(resp); // clone it because it is used to create a new element. 
+					}
+					return resp;
+				}
+			}
+		}
+		return null;
+	}
+
+	
+
+	
+	public void build()
+	{
+		if (insertionPoint==null)
+			return; 
+		else
+		{
+			Responsibility def = getFirstResponsibility();
+			PathNode oldPn = getFirstPathNode();
+
+			// this must only be executed when actually doing the initial insert. otherwise create bogus items that are not used
+			ModelCreationFactory factory;
+			PathNode newPathNode=null;
+			if (def!=null) {
+				factory = new ModelCreationFactory(targetUrn, RespRef.class, def);
+				newPathNode= (RespRef)factory.getNewObject();
+			} else
+			{
+				if (oldPn!=null)
+				{
+					newPathNode = (PathNode) EcoreUtil.copy(oldPn); 
+				}
+			}
+			
+			if (newPathNode!=null) {
+				
+				// ensures that created branches are at the right place. 
+				if (insertionPoint instanceof PathNode) {
+					newPathNode.setX(((PathNode) insertionPoint).getX());
+					newPathNode.setY(((PathNode) insertionPoint).getY());
+				} else if (insertionPoint instanceof NodeConnection)
+				{
+					newPathNode.setX(nodeConnectionMiddle.x);
+					newPathNode.setY(nodeConnectionMiddle.y);
+				}
+
+				if (newPathNode instanceof AndFork || newPathNode instanceof OrFork || newPathNode instanceof AndJoin || newPathNode instanceof OrJoin || newPathNode instanceof Stub || newPathNode instanceof Timer) 
+				{
+					DividePathCommand cmd = null;
+					if (insertionPoint instanceof EmptyPoint) {
+						cmd = new DividePathCommand(newPathNode, (EmptyPoint)insertionPoint);
+					}
+					else if (insertionPoint instanceof DirectionArrow)
+						cmd = new DividePathCommand(newPathNode, (DirectionArrow)insertionPoint);
+					else if (insertionPoint instanceof NodeConnection)
+						cmd = new DividePathCommand(newPathNode, (NodeConnection)insertionPoint, nodeConnectionMiddle.x, nodeConnectionMiddle.y);
+					
+					if (cmd!=null && oldPn!=null)
+						cmd.cloneBranchesFrom(oldPn, targetMap);
+					if (cmd!=null)
+						add(cmd);
+				} 
+				else 
+				{
+					Condition cond= null;
+					if (oldPn.getSucc().size()>0)
+					{
+						NodeConnection nc = (NodeConnection)oldPn.getSucc().get(0);
+						cond = nc.getCondition();
+					}
+					
+					Command cmd=null;
+					if (insertionPoint instanceof PathNode)
+						cmd = new ReplaceEmptyPointCommand((PathNode) insertionPoint, newPathNode, cond );
+					else if (insertionPoint instanceof NodeConnection)
+			            cmd = new SplitLinkCommand(targetMap, newPathNode, (NodeConnection)insertionPoint, nodeConnectionMiddle.x, nodeConnectionMiddle.y, cond) ;
+					
+					if (cmd!=null)
+						add(cmd);
+				}
+			}
+
+		}		
+	}
+	
+
 }
