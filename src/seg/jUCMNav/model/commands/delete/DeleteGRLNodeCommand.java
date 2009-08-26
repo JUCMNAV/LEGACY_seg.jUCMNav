930202673b2f7cd1de3bc18012e2ff482f592c7b
jfroy <jfroy@1d5a7336-c65e-6947-a78c-d4236001c669>
2009-08-26 02:58:34 +0000

    Fix the failing test case
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1601 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/model/commands/delete/DeleteGRLNodeCommand.java
+++ src/seg/jUCMNav/model/commands/delete/DeleteGRLNodeCommand.java
@@ -5,6 +5,7 @@ package seg.jUCMNav.model.commands.delete;
 
 import grl.GRLNode;
 import grl.IntentionalElementRef;
+import grl.kpimodel.KPIInformationElementRef;
 
 import org.eclipse.gef.commands.CompoundCommand;
 
@@ -38,16 +39,29 @@ public class DeleteGRLNodeCommand extends CompoundCommand {
      */
     public void execute()
     {
-    	IntentionalElementRef intElement = (IntentionalElementRef)ref;
-    	
-        //Verify if this reference is the only one
-        if(intElement.getDef().getRefs().size() == 1 &&
-        		DeletePreferences.getDeleteDefinition(ref) && ref instanceof IntentionalElementRef)
-        {
-    		//Add a command to delete the intentional element before execution
-    		add(new DeleteIntentionalElementCommand(intElement.getDef()));
-        }
-        super.execute();
+    	if(ref instanceof IntentionalElementRef)
+    	{
+    		
+	    	IntentionalElementRef intElement = (IntentionalElementRef)ref;
+	    	
+	        //Verify if this reference is the only one
+	        if(intElement.getDef().getRefs().size() == 1 &&
+	        		DeletePreferences.getDeleteDefinition(ref))
+	        {
+	    		//Add a command to delete the intentional element before execution
+	    		add(new DeleteIntentionalElementCommand(intElement.getDef()));
+	        }
+    	} else if (ref instanceof KPIInformationElementRef)
+    	{
+    		KPIInformationElementRef element = (KPIInformationElementRef)ref;
+    		//Verify if this reference is the only one
+	        if(element.getDef().getRefs().size() == 1 &&
+	        		DeletePreferences.getDeleteDefinition(element))
+	        {
+	        	add(new DeleteKPIInformationElementCommand(element.getDef()));
+	        }
+    	}
+	    super.execute();
 
     }
 
