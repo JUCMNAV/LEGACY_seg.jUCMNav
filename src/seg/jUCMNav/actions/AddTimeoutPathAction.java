058e997aeffe5f5521314ba4738f432ac51af7d5
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2009-09-14 17:23:01 +0000

    bug 693: could not add timeout path via contextual menu when timer was connected.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1609 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/actions/AddTimeoutPathAction.java
+++ src/seg/jUCMNav/actions/AddTimeoutPathAction.java
@@ -1,38 +1,76 @@
 package seg.jUCMNav.actions;
 
+import org.eclipse.gef.commands.Command;
 import org.eclipse.ui.IWorkbenchPart;
 
 import seg.jUCMNav.JUCMNavPlugin;
+import seg.jUCMNav.model.commands.create.AddBranchCommand;
+import ucm.map.NodeConnection;
+import ucm.map.Timer;
 
 /**
  * Given a timer without a timeout path, adds it.
  * 
  * @author jkealey
- *  
+ * 
  */
 public class AddTimeoutPathAction extends AddBranchAction {
 
-    public static final String ADDTIMEOUTPATH = "seg.jUCMNav.AddTimeoutPath"; //$NON-NLS-1$
+	public static final String ADDTIMEOUTPATH = "seg.jUCMNav.AddTimeoutPath"; //$NON-NLS-1$
 
-    /**
-     * @param part
-     */
-    public AddTimeoutPathAction(IWorkbenchPart part) {
-        super(part);
-        setId(ADDTIMEOUTPATH);
-    }
+	/**
+	 * @param part
+	 */
+	public AddTimeoutPathAction(IWorkbenchPart part) {
+		super(part);
+		setId(ADDTIMEOUTPATH);
+	}
 
-    /**
-     * Can add timeout path on a timer that doesn't currently have one. Method also sets the icon.
-     */
-    protected boolean calculateEnabled() {
+	/**
+	 * Can add timeout path on a timer that doesn't currently have one. Method
+	 * also sets the icon.
+	 */
+	protected boolean calculateEnabled() {
 
-        SelectionHelper sel = new SelectionHelper(getSelectedObjects());
-        if (sel.getTimer() != null) {
-            setImageDescriptor(JUCMNavPlugin.getImageDescriptor( "icons/Timer16.gif")); //$NON-NLS-1$
-            return sel.getTimer().getSucc().size() == 1;
-        } else
-            return false;
-    }
+		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
+		switch (sel.getSelectionType()) {
+		case SelectionHelper.CONNECT: {
+			if (sel.getConnect().getSucc().size() > 0
+					&& ((NodeConnection) sel.getConnect().getSucc().get(0))
+							.getTarget() instanceof Timer) {
+				Timer timer = (Timer) ((NodeConnection) sel.getConnect()
+						.getSucc().get(0)).getTarget();
+				return timer.getSucc().size() == 1;
+			}
+			break;
+		}
+		case SelectionHelper.TIMER: {
+			setImageDescriptor(JUCMNavPlugin
+					.getImageDescriptor("icons/Timer16.gif")); //$NON-NLS-1$
+			return sel.getTimer().getSucc().size() == 1;
+		}
+		}
+		return false;
+	}
+
+	protected Command getCommand() {
+		SelectionHelper sel = new SelectionHelper(getSelectedObjects());
+		switch (sel.getSelectionType()) {
+		case SelectionHelper.CONNECT: {
+			if (sel.getConnect().getSucc().size() > 0
+					&& ((NodeConnection) sel.getConnect().getSucc().get(0))
+							.getTarget() instanceof Timer) {
+				Timer timer = (Timer) ((NodeConnection) sel.getConnect()
+						.getSucc().get(0)).getTarget();
+				return new AddBranchCommand(timer);
+			}
+			break;
+		}
+		case SelectionHelper.TIMER: {
+			return new AddBranchCommand(sel.getTimer());
+		}
+		}
+		return null;
+	}
 
 }
