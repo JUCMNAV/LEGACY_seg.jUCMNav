4ed2a1b5a6ceba23401d41f1c33eeb4579e60e19
amiga <amiga@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-02-25 01:17:25 +0000

    Added ability to update stereotype references.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2162 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/actions/AddStereotypeDefinitionsAction.java
+++ src/seg/jUCMNav/actions/AddStereotypeDefinitionsAction.java
@@ -1,9 +1,13 @@
 package seg.jUCMNav.actions;
 
 import java.util.Iterator;
+import java.util.Vector;
 
 import org.eclipse.gef.commands.Command;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 
 import seg.jUCMNav.JUCMNavPlugin;
 import seg.jUCMNav.Messages;
@@ -19,9 +23,21 @@ import urncore.Metadata;
  * @author amiga
  */
 
+//name="StereotypeDef", value="ST_CLASSTYPE,CLASS1,IntentionalElement"
+//name="StereotypeDef", value="ST_CLASSTYPE,CLASS2,IntentionalElement"
+//name="StereotypeDef", value="ST_CLASSTYPE,OTHER,IntentionalElement"
+//name="StereotypeDef", value="acceptStereotype,CLASS1,EvaluationStrategy"
+//name="StereotypeDef", value="acceptStereotype,CLASS2,EvaluationStrategy"
+//name="StereotypeDef", value="acceptStereotype,OTHER,EvaluationStrategy"
+
+
 public class AddStereotypeDefinitionsAction extends URNSelectionAction {
 
     public static final String ADD_STEREOTYPE_DEFINITIONS = "seg.jUCMNav.AddStereotypeDefinitions"; //$NON-NLS-1$
+
+    private String [] values = { "ST_CLASSTYPE,CLASS1,IntentionalElement", "ST_CLASSTYPE,CLASS2,IntentionalElement", "ST_CLASSTYPE,OTHER,IntentionalElement",
+    		"acceptStereotype,CLASS1,EvaluationStrategy", "acceptStereotype,CLASS2,EvaluationStrategy", "acceptStereotype,OTHER,EvaluationStrategy"
+    };
 
 	public AddStereotypeDefinitionsAction(IWorkbenchPart part) {
 		super(part);
@@ -37,33 +53,83 @@ public class AddStereotypeDefinitionsAction extends URNSelectionAction {
 
         if( urnspec == null )
         	return false;
-        
-        boolean hasExistingDefinitions = this.hasStereotypeDefinitions(urnspec);
-        
-        if( JUCMNavPlugin.isInDebug() ) System.out.println( "hasExistingDefinitions = " + hasExistingDefinitions );
-        
-        return ( !hasExistingDefinitions );
+        else
+        	return true;
     }
 
-     protected Command getCommand() {
+     protected Command getCommand()
+     {
+ 		String title, message, commandLabel;
+ 		Metadata [] mdList, sdList;
+ 		
+		final int CANCEL = 0;
+		final int UPDATE = 1;
+		int userChoice;
+		
         SelectionHelper sel = new SelectionHelper(getSelectedObjects());
-        
         URNspec urnspec = sel.getUrnspec();
         
-        if( this.hasStereotypeDefinitions(urnspec))
+        if( urnspec == null )
         	return null;
         
-        Metadata [] sdList = this.getStereotypeDefinitions(urnspec);
-        Metadata [] mdList = (Metadata[]) urnspec.getMetadata().toArray(new Metadata[0]);
+        if( this.hasStereotypeDefinitions(urnspec)) { // inform user of existing definitions and provide option to cancel 
+        
+            // remove previous definitions by making a list of all other URNspec metadata
+            Vector<Metadata> otherMetadata = new Vector<Metadata>();
+            Vector<Metadata> existingMetadata = new Vector<Metadata>();
+            
+        	if( urnspec.getMetadata().size() > 0 ) {
+        		for( Iterator iter = urnspec.getMetadata().iterator(); iter.hasNext();) {
+        			Metadata md = (Metadata) iter.next();	
+        			if( md.getName().equalsIgnoreCase( "StereotypeDef" )){ //$NON-NLS-1$
+        				existingMetadata.add(md);
+        			} else {
+        				otherMetadata.add(md);        				
+        			}
+        		}    		
+        	}
+        	
+			StringBuilder messageBuf = new StringBuilder();
+			messageBuf.append( "A set of stereotype definitions already exists in the current model.\n\n" );
+        
+			for( Metadata em : existingMetadata ) {
+				messageBuf.append( "\t" + em.getValue() + "\n" );
+			}
+			
+			messageBuf.append( "\nDo you wish to update these definitions ?");
+			
+			title = "Stereotype Definitions Exist";
+			message = messageBuf.toString();
+			
+			final String[] labels = { "Cancel", "Update Stereotype Definitions" };
+
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			MessageDialog md = new MessageDialog( shell, title, null, message, MessageDialog.QUESTION, labels, UPDATE );
+			md.create();
+			userChoice = md.open();
+
+			if( userChoice == CANCEL) {
+				return null;
+			}
+
+            mdList = (Metadata[]) otherMetadata.toArray(new Metadata[0]);
+        	commandLabel = Messages.getString("ActionRegistryManager.updateStereotypeDefinitions");            
+        } else {
+        	mdList = (Metadata[]) urnspec.getMetadata().toArray(new Metadata[0]);
+        	commandLabel = Messages.getString("ActionRegistryManager.addStereotypeDefinitions");
+        }
+        
+        sdList = this.getStereotypeDefinitions(urnspec);        
+        
         Metadata[] combinedList = ArrayAndListUtils.concatenateArrays( mdList, sdList );
         
-        Command cmd = new ChangeMetadataCommand( urnspec, combinedList, Messages.getString("ActionRegistryManager.addStereotypeDefinitions") );
+        Command cmd = new ChangeMetadataCommand( urnspec, combinedList, commandLabel );
         
         return cmd;
     }
     
-    protected boolean hasStereotypeDefinitions( URNspec urnspec ) {
-    	
+    protected boolean hasStereotypeDefinitions( URNspec urnspec )
+    {	
     	if( urnspec.getMetadata().size() > 0 ) {
     		for( Iterator iter = urnspec.getMetadata().iterator(); iter.hasNext();) {
     			Metadata md = (Metadata) iter.next();	
@@ -76,19 +142,9 @@ public class AddStereotypeDefinitionsAction extends URNSelectionAction {
     	return false;
     }
     
-    protected Metadata [] getStereotypeDefinitions( URNspec urnspec ) {    	
-//    	name="StereotypeDef", value="ST_CLASSTYPE,CLASS1,IntentionalElement"
-//    	name="StereotypeDef", value="ST_CLASSTYPE,CLASS2,IntentionalElement"
-//    	name="StereotypeDef", value="ST_CLASSTYPE,OTHER,IntentionalElement"
-//    	name="StereotypeDef", value="acceptStereotype,CLASS1,EvaluationStrategy"
-//    	name="StereotypeDef", value="acceptStereotype,CLASS2,EvaluationStrategy"
-//    	name="StereotypeDef", value="acceptStereotype,OTHER,EvaluationStrategy"
-
-    	String [] values = { "ST_CLASSTYPE,CLASS1,IntentionalElement", "ST_CLASSTYPE,CLASS2,IntentionalElement", "ST_CLASSTYPE,OTHER,IntentionalElement",
-    			"acceptStereotype,CLASS1,EvaluationStrategy", "acceptStereotype,CLASS2,EvaluationStrategy", "acceptStereotype,OTHER,EvaluationStrategy"
-    	};
-    	
-    	// populate with stereotypes
+    protected Metadata [] getStereotypeDefinitions( URNspec urnspec )
+    {    		
+    	// populate new list with stereotypes
         Metadata [] mdList = new Metadata[values.length];
         Metadata newMetadata;
         for( int i = 0; i < values.length; i++ ) {
@@ -100,5 +156,4 @@ public class AddStereotypeDefinitionsAction extends URNSelectionAction {
         
         return mdList;
     }
-
 }
