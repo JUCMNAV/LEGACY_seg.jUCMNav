8ce0291f2511dbc6daaf1bd873e6825a4c482f9b
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-04-05 14:53:06 +0000

    bug 775: remove trailing on single lines as well.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2456 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/views/property/tabbed/sections/NamePropertySection.java
+++ src/seg/jUCMNav/views/property/tabbed/sections/NamePropertySection.java
@@ -27,70 +27,61 @@ public class NamePropertySection extends AbstractMultiLineStringPropertySection 
     public String getLabelText() {
         return Messages.getString("NamePropertySection_Name"); //$NON-NLS-1$
     }
-    
+
     protected boolean isTextValid(String text) {
         boolean result = true;
-        
+
         /*
-        if (eObject instanceof Responsibility) {
-            result = !URNNamingHelper.doesResponsibilityNameExists(((Responsibility) eObject).getUrndefinition().getUrnspec(), text)
-                      || ((Responsibility)eObject).getName().equals(text);
-        } else if(eObject instanceof Component) {
-            result = !URNNamingHelper.doesComponentNameExists(((Component) eObject).getUrndefinition().getUrnspec(), text)
-                    || ((Component)eObject).getName().equals(text);
-        } else if(eObject instanceof Actor) {
-            result = !URNNamingHelper.doesActorNameExists(((Actor) eObject).getGrlspec().getUrnspec(), text)
-                    || ((Actor)eObject).getName().equals(text);
-        } else if(eObject instanceof Variable) {
-            result = !URNNamingHelper.doesVariableNameExist(((Variable) eObject).getUcmspec().getUrnspec(), text)
-                    || ((Variable)eObject).getName().equals(text);
-        } else if(eObject instanceof IntentionalElement) {
-            result = !URNNamingHelper.doesIntentionalElementNameExists(((IntentionalElement) eObject).getGrlspec().getUrnspec(), text)
-                    || ((IntentionalElement)eObject).getName().equals(text);
-        } else if(eObject instanceof KPIInformationElement) {
-            result = !URNNamingHelper.doesKPIInformationElementNameExists(((KPIInformationElement) eObject).getGrlspec().getUrnspec(), text)
-                    || ((KPIInformationElement) eObject).getName().equals(text);
-        }
-        
-        if(!result)
-           MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.getString("NamePropertySection.NameAlreadyExists"), Messages.getString("NamePropertySection.ThisNameConflicts")); //$NON-NLS-1$ //$NON-NLS-2$
-        */
-        if (!(eObject instanceof URNmodelElement)) return false;
-        
+         * if (eObject instanceof Responsibility) { result = !URNNamingHelper.doesResponsibilityNameExists(((Responsibility)
+         * eObject).getUrndefinition().getUrnspec(), text) || ((Responsibility)eObject).getName().equals(text); } else if(eObject instanceof Component) { result
+         * = !URNNamingHelper.doesComponentNameExists(((Component) eObject).getUrndefinition().getUrnspec(), text) ||
+         * ((Component)eObject).getName().equals(text); } else if(eObject instanceof Actor) { result = !URNNamingHelper.doesActorNameExists(((Actor)
+         * eObject).getGrlspec().getUrnspec(), text) || ((Actor)eObject).getName().equals(text); } else if(eObject instanceof Variable) { result =
+         * !URNNamingHelper.doesVariableNameExist(((Variable) eObject).getUcmspec().getUrnspec(), text) || ((Variable)eObject).getName().equals(text); } else
+         * if(eObject instanceof IntentionalElement) { result = !URNNamingHelper.doesIntentionalElementNameExists(((IntentionalElement)
+         * eObject).getGrlspec().getUrnspec(), text) || ((IntentionalElement)eObject).getName().equals(text); } else if(eObject instanceof
+         * KPIInformationElement) { result = !URNNamingHelper.doesKPIInformationElementNameExists(((KPIInformationElement) eObject).getGrlspec().getUrnspec(),
+         * text) || ((KPIInformationElement) eObject).getName().equals(text); }
+         * 
+         * if(!result) MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+         * Messages.getString("NamePropertySection.NameAlreadyExists"), Messages.getString("NamePropertySection.ThisNameConflicts")); //$NON-NLS-1$
+         * //$NON-NLS-2$
+         */
+        if (!(eObject instanceof URNmodelElement))
+            return false;
+
         URNspec urn = null;
-        EObject parent = eObject; 
+        EObject parent = eObject;
         do {
             if (parent instanceof URNspec) {
                 urn = (URNspec) parent;
                 break;
             }
-            if (parent!=null)
+            if (parent != null)
                 parent = parent.eContainer();
-            
+
         } while (parent != null);
-        
-       
+
         String message = URNNamingHelper.isNameValid(urn, (URNmodelElement) eObject, text);
 
         result = message.length() == 0;
-        
+
         if (!result) {
             MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error", message); //$NON-NLS-1$
         }
-        
+
         return result;
     }
-    
+
     protected Object getFeatureValue(String newText) {
-        String newline = System.getProperty("line.separator", "\n"); 
-        if (eObject instanceof Responsibility || eObject instanceof RespRef || eObject instanceof Stub) 
-        {
-            while (newText.endsWith(newline))
-                newText = newText.substring(0, newText.length() - newline.length());
-            
-            return newText; // multiline is allowed, but remove trailing newline.  
-        }
-        else
-            return newText.replaceAll(newline, " "); // single line. 
+        String newline = System.getProperty("line.separator", "\n");
+        // remove trailing.
+        while (newText.endsWith(newline))
+            newText = newText.substring(0, newText.length() - newline.length());
+
+        if (eObject instanceof Responsibility || eObject instanceof RespRef || eObject instanceof Stub) {
+            return newText; // multiline is allowed, but remove trailing newline.
+        } else
+            return newText.replaceAll(newline, " "); // single line.
     }
 }
