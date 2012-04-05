e67a20ae667b0fc779b54984394d2252ae48ceb8
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-04-05 01:40:15 +0000

    bug 823: selection highlighting bug.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2451 1d5a7336-c65e-6947-a78c-d4236001c669

--- src/seg/jUCMNav/views/strategies/StrategiesView.java
+++ src/seg/jUCMNav/views/strategies/StrategiesView.java
@@ -701,8 +701,11 @@ public class StrategiesView extends ViewPart implements IPartListener2, ISelecti
             for (int i = 0; i < multieditor.getPageCount(); i++) {
                 UrnEditor u = (UrnEditor) multieditor.getEditor(i);
                 ((URNRootEditPart) u.getGraphicalViewer().getRootEditPart()).setStrategyView(false);
+                ((URNRootEditPart) u.getGraphicalViewer().getRootEditPart()).setScenarioView(true);
             }
         }
+        
+        viewer.deselectAll();
     }
 
     public Object getAdapter(Class adapter) {
