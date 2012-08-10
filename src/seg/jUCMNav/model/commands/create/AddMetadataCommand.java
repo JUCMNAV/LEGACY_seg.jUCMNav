3343e1792f29bf98abb1a2a54f510fb508503121
rouzbahan <rouzbahan@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-08-10 16:22:55 +0000

    Adding the missing file.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2556 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ src/seg/jUCMNav/model/commands/create/AddMetadataCommand.java
@@ -0,0 +1,106 @@
+package seg.jUCMNav.model.commands.create;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.commands.Command;
+
+import seg.jUCMNav.Messages;
+import seg.jUCMNav.model.commands.JUCMNavCommand;
+import urn.URNspec;
+import urncore.Metadata;
+import urncore.URNmodelElement;
+
+/**
+ * adds the metadata associated with a urn model element.
+ * 
+ * @author rouzbahan
+ */
+public class AddMetadataCommand extends Command implements JUCMNavCommand {
+    private URNmodelElement urnelem = null;
+    private URNspec urnspec = null;
+    private Metadata[] oldMetadataArray;
+    private Metadata[] newMetadataArray;
+
+    public AddMetadataCommand(EObject obj, Metadata metadataArray, String label) {
+        if ( (obj instanceof URNmodelElement) || (obj instanceof URNspec) ) {
+            
+            if(obj instanceof URNmodelElement)
+                this.urnelem = (URNmodelElement) obj;
+            else
+                this.urnspec = (URNspec) obj;
+            
+            //this.newMetadataArray = metadataArray;
+            
+            /*if( label != null)
+                setLabel( label );
+            else
+                setLabel(Messages.getString("ChangeMetadataCommand.ChangeURNmodelElementMetadata")); //$NON-NLS-1$*/
+        }
+    }
+
+    /**
+     * @see org.eclipse.gef.commands.Command#execute()
+     */
+    public void execute() {
+        /*EList metadata = this.getMetadata();
+        oldMetadataArray = (Metadata[]) metadata.toArray(new Metadata[0]);
+        redo();*/
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.commands.Command#redo()
+     */
+    public void redo() {
+        /*testPreConditions();
+
+        EList metadata = this.getMetadata();
+        metadata.clear();
+        for (int i = 0; i < newMetadataArray.length; i++) {
+            metadata.add(newMetadataArray[i]);
+        }
+
+        testPostConditions();*/
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPostConditions()
+     */
+    public void testPostConditions() {
+        //assert urnelem != null || urnspec != null : "post no element to name!"; //$NON-NLS-1$
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see seg.jUCMNav.model.commands.JUCMNavCommand#testPreConditions()
+     */
+    public void testPreConditions() {
+        //assert urnelem != null || urnspec != null : "pre no element to name!"; //$NON-NLS-1$
+    }
+
+    /**
+     * @see org.eclipse.gef.commands.Command#undo()
+     */
+    public void undo() {
+        /*testPostConditions();
+
+        EList metadata = this.getMetadata();
+        metadata.clear();
+        for (int i = 0; i < oldMetadataArray.length; i++) {
+            metadata.add(oldMetadataArray[i]);
+        }
+
+        testPreConditions();*/
+    }
+    
+    private EList getMetadata() {
+        if( urnelem != null )
+            return( urnelem.getMetadata() );
+        else
+            return( urnspec.getMetadata() );
+    }
+}
