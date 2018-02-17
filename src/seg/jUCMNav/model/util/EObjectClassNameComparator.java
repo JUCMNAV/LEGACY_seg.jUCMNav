fd879ff9d0f6d3a96d32f24410e3abee47e57292
Sahil Luthra <sahil.luthra1990@gmail.com>
2018-02-16 19:26:37 -0500

    Organize imports and providing a naming procedure for the different ucm elements

--- src/seg/jUCMNav/model/util/EObjectClassNameComparator.java
+++ src/seg/jUCMNav/model/util/EObjectClassNameComparator.java
@@ -1,11 +1,5 @@
 package seg.jUCMNav.model.util;
 
-import grl.Actor;
-import grl.ActorRef;
-import grl.Contribution;
-import grl.IntentionalElementRef;
-import grl.kpimodel.KPIInformationElementRef;
-
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -13,15 +7,27 @@ import java.util.Comparator;
 
 import org.eclipse.emf.ecore.EObject;
 
+import grl.Actor;
+import grl.ActorRef;
+import grl.Contribution;
+import grl.ContributionChange;
+import grl.IntentionalElementRef;
+import grl.kpimodel.KPIInformationElementRef;
 import ucm.map.ComponentRef;
 import ucm.map.DirectionArrow;
 import ucm.map.EmptyPoint;
+import ucm.map.FailurePoint;
+import ucm.map.NodeConnection;
+import ucm.map.OrFork;
+import ucm.map.PluginBinding;
 import ucm.map.RespRef;
 import ucm.performance.Demand;
 import urn.dyncontext.Change;
 import urn.dyncontext.DeactivationChange;
 import urn.dyncontext.PropertyChange;
+import urn.dyncontext.TextChange;
 import urncore.Component;
+import urncore.URNmodelElement;
 
 /**
  * Orders EObjects by class name then by element name.
@@ -31,132 +37,144 @@ import urncore.Component;
  */
 public class EObjectClassNameComparator implements Comparator, Serializable {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Sorts alphabetically by class name, except for DirectionArrows and EmptyPoints that are put at the end. Then, if still equal, sorts using
-     * getSortableElementName()
-     * 
-     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-     * @see #getSortableElementName(EObject)
-     */
-    public int compare(Object arg0, Object arg1) {
+	/**
+	 * Sorts alphabetically by class name, except for DirectionArrows and EmptyPoints that are put at the end. Then, if still equal, sorts using
+	 * getSortableElementName()
+	 * 
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 * @see #getSortableElementName(EObject)
+	 */
+	public int compare(Object arg0, Object arg1) {
 
-        try {
-            if (arg0 instanceof String && arg1 instanceof String)
-                return ((String) arg0).compareTo((String) arg1);
+		try {
+			if (arg0 instanceof String && arg1 instanceof String)
+				return ((String) arg0).compareTo((String) arg1);
 
-            // sort by classes first
-            int i = compareClass((EObject) arg0, (EObject) arg1);
+			// sort by classes first
+			int i = compareClass((EObject) arg0, (EObject) arg1);
 
-            if (i != 0)
-                return i;
-            else {
-                // then by name if same class.
-                return getSortableElementName((EObject) arg0).compareToIgnoreCase(getSortableElementName((EObject) arg1));
-            }
+			if (i != 0)
+				return i;
+			else {
+				// then by name if same class.
+				return getSortableElementName((EObject) arg0).compareToIgnoreCase(getSortableElementName((EObject) arg1));
+			}
 
-        } catch (Exception ex) {
-            return 0;
-        }
-    }
+		} catch (Exception ex) {
+			return 0;
+		}
+	}
 
-    /**
-     * Returns a name to be used in the sort procedure. ComponentRefs/RespRefs/IntentionalElementRefs use definition name, others uses the "name" attribute if
-     * it exists and the class name otherwise.
-     * 
-     * @param o
-     *            the object for which to obtain the name
-     * @return the name
-     */
-    public static String getSortableElementName(EObject o) {
+	/**
+	 * Returns a name to be used in the sort procedure. ComponentRefs/RespRefs/IntentionalElementRefs use definition name, others uses the "name" attribute if
+	 * it exists and the class name otherwise.
+	 * 
+	 * @param o
+	 *            the object for which to obtain the name
+	 * @return the name
+	 */
+	public static String getSortableElementName(EObject o) {
 
-        String s;
-        // want to build a string like so: Name (ID)
-        // but have to deal with special cases
+		String s;
+		// want to build a string like so: Name (ID)
+		// but have to deal with special cases
 
-        if (o instanceof ComponentRef && ((ComponentRef) o).getContDef() != null) {
-            s = ((Component) ((ComponentRef) o).getContDef()).getName();
-        } else if (o instanceof ActorRef && ((ActorRef) o).getContDef() != null) {
-            s = ((Actor) ((ActorRef) o).getContDef()).getName();
-        } else if (o instanceof RespRef && ((RespRef) o).getRespDef() != null) {
-            s = ((RespRef) o).getRespDef().getName();
-        } else if (o instanceof IntentionalElementRef && ((IntentionalElementRef) o).getDef() != null) {
-            s = ((IntentionalElementRef) o).getDef().getName();
-        } else if (o instanceof KPIInformationElementRef && ((KPIInformationElementRef) o).getDef() != null) {
-            s = ((KPIInformationElementRef) o).getDef().getName();
-        } else if (o instanceof Demand) {
-            if (((Demand) o).getResponsibility() != null)
-                s = getSortableElementName(((Demand) o).getResponsibility());
-            else
-                s = o.eClass().getName();
-        } else if (o instanceof Change) {                                 //Build name for a Change
-        	String property = null;
-        	Change change = (Change) o;
-        	DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        	if (change instanceof DeactivationChange)
-        		property = "Deactive";
-        	else
-        		property = ((PropertyChange) change).getAffectedProperty();
-        	if (change.getElement() instanceof Contribution)
-        		s = change.getElement().getName() + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
-        	else
-        		s = URNNamingHelper.getName(change.getElement()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
-        } else {
-            try {
-                Object name = o.eGet(o.eClass().getEStructuralFeature("name")); //$NON-NLS-1$
-                if (name == null)
-                    s = ""; //$NON-NLS-1$
-                else
-                    s = name.toString();
-            } catch (Exception ex) {
-                s = o.eClass().getName();
-            }
+		if (o instanceof ComponentRef && ((ComponentRef) o).getContDef() != null) {
+			s = ((Component) ((ComponentRef) o).getContDef()).getName();
+		} else if (o instanceof ActorRef && ((ActorRef) o).getContDef() != null) {
+			s = ((Actor) ((ActorRef) o).getContDef()).getName();
+		} else if (o instanceof RespRef && ((RespRef) o).getRespDef() != null) {
+			s = ((RespRef) o).getRespDef().getName();
+		} else if (o instanceof IntentionalElementRef && ((IntentionalElementRef) o).getDef() != null) {
+			s = ((IntentionalElementRef) o).getDef().getName();
+		} else if (o instanceof KPIInformationElementRef && ((KPIInformationElementRef) o).getDef() != null) {
+			s = ((KPIInformationElementRef) o).getDef().getName();
+		} else if (o instanceof Demand) {
+			if (((Demand) o).getResponsibility() != null)
+				s = getSortableElementName(((Demand) o).getResponsibility());
+			else
+				s = o.eClass().getName();
+		} else if (o instanceof Change) {                                 //Build name for a Change
+			String property = null;
+			Change change = (Change) o;
+			DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+			if (change instanceof DeactivationChange)
+				property = "Deactive";
+			else
+				property = ((PropertyChange) change).getAffectedProperty();
+			if (change.getElement() instanceof Contribution)
+				s = ((URNmodelElement) change.getElement()).getName() + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+			else if (change.getElement() instanceof ContributionChange)
+				s = URNNamingHelper.getName((ContributionChange) change.getElement()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+			else if (change.getElement() instanceof PluginBinding)
+				s = URNNamingHelper.getName((PluginBinding) change.getElement()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+			else if (change.getElement() instanceof urncore.Condition) {
+				if (((TextChange) change).getAffectedProperty().startsWith("Expression"))
+					s = URNNamingHelper.getName((OrFork)((NodeConnection)(((urncore.Condition) change.getElement()).eContainer())).getSource()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+				else if (((TextChange) change).getAffectedProperty().equals("Failure Condition"))
+					s = URNNamingHelper.getName((FailurePoint)((NodeConnection)(((urncore.Condition) change.getElement()).eContainer())).getSource()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+				else
+					s = URNNamingHelper.getName((urncore.Condition) change.getElement()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+			}
+			else
+				s = URNNamingHelper.getName((URNmodelElement) change.getElement()) + " (" + property + ") " + df.format(change.getStart()) + " -> " + df.format(change.getEnd());
+		} else {
+			try {
+				Object name = o.eGet(o.eClass().getEStructuralFeature("name")); //$NON-NLS-1$
+				if (name == null)
+					s = ""; //$NON-NLS-1$
+				else
+					s = name.toString();
+			} catch (Exception ex) {
+				s = o.eClass().getName();
+			}
 
-        }
+		}
 
-        try {
-            Object id = o.eGet(o.eClass().getEStructuralFeature("id")); //$NON-NLS-1$
-            if (id != null) {
-                s += " (" + id.toString() + ")"; //$NON-NLS-1$ //$NON-NLS-2$ 
-            }
-        } catch (Exception ex) {
-        }
+		try {
+			Object id = o.eGet(o.eClass().getEStructuralFeature("id")); //$NON-NLS-1$
+			if (id != null) {
+				s += " (" + id.toString() + ")"; //$NON-NLS-1$ //$NON-NLS-2$ 
+			}
+		} catch (Exception ex) {
+		}
 
-        return s;
-    }
+		return s;
+	}
 
-    /**
-     * Compares elements by class type. First sorts using getClassRank() and then using the class name.
-     * 
-     * @see #getClassRank(Object)
-     */
-    private int compareClass(EObject arg0, EObject arg1) {
-        int i = getClassRank(arg0) - getClassRank(arg1);
+	/**
+	 * Compares elements by class type. First sorts using getClassRank() and then using the class name.
+	 * 
+	 * @see #getClassRank(Object)
+	 */
+	private int compareClass(EObject arg0, EObject arg1) {
+		int i = getClassRank(arg0) - getClassRank(arg1);
 
-        if (i != 0)
-            return i;
-        else {
-            return arg0.eClass().getInstanceClassName().compareToIgnoreCase(arg1.eClass().getInstanceClassName());
-        }
-    }
+		if (i != 0)
+			return i;
+		else {
+			return arg0.eClass().getInstanceClassName().compareToIgnoreCase(arg1.eClass().getInstanceClassName());
+		}
+	}
 
-    /**
-     * Associates a rank to model element classes. In a sorted list, elements should be sorted by ascending rank.
-     * 
-     * Divides elements into two groups (DirectionArrows and EmptyPoints) versus the rest.
-     * 
-     * @param o
-     *            the object for which to obtain the rank
-     * @return rank the rank itself. 1 for DirectionArrows and EmptyPoints, 0 otherwise.
-     */
-    private int getClassRank(Object o) {
-        if (o instanceof EmptyPoint || o instanceof DirectionArrow) {
-            return 1;
-        } else
-            return 0;
-    }
+	/**
+	 * Associates a rank to model element classes. In a sorted list, elements should be sorted by ascending rank.
+	 * 
+	 * Divides elements into two groups (DirectionArrows and EmptyPoints) versus the rest.
+	 * 
+	 * @param o
+	 *            the object for which to obtain the rank
+	 * @return rank the rank itself. 1 for DirectionArrows and EmptyPoints, 0 otherwise.
+	 */
+	private int getClassRank(Object o) {
+		if (o instanceof EmptyPoint || o instanceof DirectionArrow) {
+			return 1;
+		} else
+			return 0;
+	}
 }
