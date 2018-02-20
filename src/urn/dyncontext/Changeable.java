37508c95a6eadd72d1317e7ae17835db65aa5eed
Sahil Luthra <sahil.luthra1990@gmail.com>
2018-02-19 20:19:24 -0500

    Created a new representation of the model object 'Changeable'

---
+++ src/urn/dyncontext/Changeable.java
@@ -0,0 +1,20 @@
+/**
+ */
+package urn.dyncontext;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Changeable</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see urn.dyncontext.DyncontextPackage#getChangeable()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Changeable extends EObject {
+
+
+} // Changeable
