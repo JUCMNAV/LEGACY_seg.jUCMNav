194cb0aae8ed56fee437363329d0fb29cc01cb89
Sahil Luthra <sahil.luthra1990@gmail.com>
2018-02-19 19:59:09 -0500

    Created a new implementation class of the model object  'Changeable'

---
+++ src/urn/dyncontext/impl/ChangeableImpl.java
@@ -0,0 +1,38 @@
+/**
+ */
+package urn.dyncontext.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import urn.dyncontext.Changeable;
+import urn.dyncontext.DyncontextPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Changeable</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class ChangeableImpl extends MinimalEObjectImpl.Container implements Changeable {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ChangeableImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected EClass eStaticClass() {
+		return DyncontextPackage.Literals.CHANGEABLE;
+	}
+
+} //ChangeableImpl
